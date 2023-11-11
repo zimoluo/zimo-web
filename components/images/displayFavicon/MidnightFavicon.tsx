43dadddb96b4a21e126cc9daab35d980606ebac3
zimoluo <zimo_luo@outlook.com>
2023-11-11 22:32:23 +0800

    randomize midnight

--- components/images/displayFavicon/MidnightFavicon.tsx
+++ components/images/displayFavicon/MidnightFavicon.tsx
@@ -1,3 +1,5 @@
+"id random";
+
 export default function MidnightFavicon({
   className = "",
   height,
@@ -23,7 +25,7 @@ export default function MidnightFavicon({
     >
       <defs>
         <linearGradient
-          id="b"
+          id="b_e98983cae6c43ac38b1e"
           x1={22.27}
           x2={1038.27}
           y1={530.27}
@@ -34,7 +36,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="d"
+          id="d_b5bf6ffbcac48aa45c8a"
           x1={216.254}
           x2={925.361}
           y1={832.966}
@@ -46,7 +48,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="e"
+          id="e_214b78073c2830a6d86c"
           x1={212.553}
           x2={945.352}
           y1={894.627}
@@ -58,7 +60,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="g"
+          id="g_c90ae50fad17d24b12c8"
           x1={10.548}
           x2={1049.99}
           y1={690.837}
@@ -69,7 +71,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="i"
+          id="i_eaafdad45f3e28183dcd"
           x1={5.314}
           x2={1052.94}
           y1={463.128}
@@ -81,7 +83,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="j"
+          id="j_c0521e4ec02844d52d1e"
           x1={-276.928}
           x2={770.698}
           y1={988.178}
@@ -93,7 +95,7 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <linearGradient
-          id="k"
+          id="k_f1710c74d605d6c4b965"
           x1={545.991}
           x2={1121.43}
           y1={1172.51}
@@ -105,28 +107,28 @@ export default function MidnightFavicon({
           <stop offset={1} stopColor="#374151" />
         </linearGradient>
         <path
-          id="a"
+          id="a_95c4996999abcb70b8f6"
           d="M22.27 530.27c0-280.561 227.439-508 508-508 280.561 0 508 227.439 508 508 0 280.561-227.439 508-508 508-280.561 0-508-227.439-508-508Z"
         />
         <path
-          id="f"
+          id="f_93be6e63982b1449894f"
           d="M526.89-56.031c-219.954.959-407.267 138.433-482.3 331.777-19.167 73.108-30.761 149.294-33.456 227.768.468 6.099.886 12.215 1.559 18.255-.64.327-1.247.709-1.886 1.04-.09 5.673-.259 11.324-.259 17.02 0 378.406 202.39 709.271 504.649 891.061 1.04-.62 2.15-1.13 3.184-1.75 5.094 3.03 10.387 5.71 15.592 8.57 308.563-180.26 516.017-514.735 516.017-897.881 0-1.24-.06-2.465-.07-3.702-1.93-1.155-3.76-2.445-5.71-3.575 2.25-17.002 3.43-34.321 4.03-51.777-3.53-62.833-12.57-124.171-26.77-183.525C951.81 91.061 756.488-57.032 526.89-56.031Z"
         />
       </defs>
       <use
-        xlinkHref="#a"
-        fill="url(#b)"
+        xlinkHref="#a_95c4996999abcb70b8f6"
+        fill="url(#b_e98983cae6c43ac38b1e)"
         stroke="#f3f4f6"
         strokeLinecap="butt"
         strokeLinejoin="miter"
         strokeWidth={40}
       />
-      <clipPath id="c" clipRule="nonzero">
-        <use xlinkHref="#a" />
+      <clipPath id="c_364399a1c3ea750aa1af" clipRule="nonzero">
+        <use xlinkHref="#a_95c4996999abcb70b8f6" />
       </clipPath>
-      <g clipPath="url(#c)">
+      <g clipPath="url(#c_364399a1c3ea750aa1af)">
         <path
-          fill="url(#d)"
+          fill="url(#d_b5bf6ffbcac48aa45c8a)"
           stroke="#f3f4f6"
           strokeLinecap="butt"
           strokeLinejoin="miter"
@@ -135,7 +137,7 @@ export default function MidnightFavicon({
           opacity={0.332}
         />
         <path
-          fill="url(#e)"
+          fill="url(#e_214b78073c2830a6d86c)"
           stroke="#f3f4f6"
           strokeLinecap="butt"
           strokeLinejoin="miter"
@@ -143,33 +145,33 @@ export default function MidnightFavicon({
           d="M526.89-56.031c-219.954.959-407.267 138.433-482.3 331.777-19.167 73.108-30.761 149.294-33.456 227.768.468 6.099.886 12.215 1.559 18.255-.64.327-1.247.709-1.886 1.04-.09 5.673-.259 11.324-.259 17.02 0 378.406 202.39 709.271 504.649 891.061 1.04-.62 2.15-1.13 3.184-1.75 5.094 3.03 10.387 5.71 15.592 8.57 308.563-180.26 516.017-514.735 516.017-897.881 0-1.24-.06-2.465-.07-3.702-1.93-1.155-3.76-2.445-5.71-3.575 2.25-17.002 3.43-34.321 4.03-51.777-3.53-62.833-12.57-124.171-26.77-183.525C951.81 91.061 756.488-57.032 526.89-56.031Z"
         />
         <use
-          xlinkHref="#f"
-          fill="url(#g)"
+          xlinkHref="#f_93be6e63982b1449894f"
+          fill="url(#g_c90ae50fad17d24b12c8)"
           stroke="#f3f4f6"
           strokeLinecap="butt"
           strokeLinejoin="miter"
           strokeWidth={40}
         />
-        <clipPath id="h" clipRule="nonzero">
-          <use xlinkHref="#f" />
+        <clipPath id="h_bc21d411cd017bf16ca8" clipRule="nonzero">
+          <use xlinkHref="#f_93be6e63982b1449894f" />
         </clipPath>
         <g
           stroke="#f3f4f6"
           strokeLinecap="butt"
           strokeLinejoin="miter"
           strokeWidth={40}
-          clipPath="url(#h)"
+          clipPath="url(#h_bc21d411cd017bf16ca8)"
         >
           <path
-            fill="url(#i)"
+            fill="url(#i_eaafdad45f3e28183dcd)"
             d="M9.42 465.917C8.17 178.886 239.84-54.813 526.872-56.063c287.031-1.251 520.728 230.42 521.978 517.451 1.25 287.031-230.416 520.729-517.448 521.981-287.032 1.25-520.73-230.42-521.982-517.452Z"
           />
           <path
-            fill="url(#j)"
+            fill="url(#j_c0521e4ec02844d52d1e)"
             d="M-270.617 986.864c-1.251-287.032 230.42-520.73 517.451-521.981 287.031-1.251 520.731 230.42 521.981 517.451 1.252 287.036-230.421 520.736-517.452 521.986-287.03 1.25-520.729-230.43-521.98-517.456Z"
           />
           <path
-            fill="url(#k)"
+            fill="url(#k_f1710c74d605d6c4b965)"
             d="M264.061 984.533c-1.25-287.031 230.421-520.729 517.452-521.981 287.027-1.25 520.737 230.421 521.987 517.452 1.25 287.036-230.42 520.726-517.456 521.976-287.033 1.26-520.731-230.42-521.983-517.447Z"
           />
         </g>
