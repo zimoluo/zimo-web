907a5cd0c65a6cabb5ad68bb42c8a4edf35c9f5a
zimoluo <zimo_luo@outlook.com>
2023-11-11 22:33:18 +0800

    awa

--- prebuild-helper/prebuild.js
+++ prebuild-helper/prebuild.js
@@ -2,6 +2,8 @@ const optimizeResource = require("./image-optimizer");
 const generateFavicon = require("./website-favicon-generator");
 const randomizeSvg = require("./svg-id-randomizer");
 
+console.log("Running custom prebuild scripts...");
+
 optimizeResource();
 generateFavicon();
 randomizeSvg();
