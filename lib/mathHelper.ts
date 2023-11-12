d34d3bd9876ddbb4c8397976fad3d99ebc134c5b
zimoluo <zimo_luo@outlook.com>
2023-11-12 15:26:16 +0800

    comment

--- lib/mathHelper.ts
+++ lib/mathHelper.ts
@@ -1,7 +1,19 @@
-export function randomIntFromRange(min: number, max: number) {
+/**
+ * Generates a random integer within a specified range, inclusive of both the minimum and maximum values.
+ * @param {number} min - The minimum value in the range.
+ * @param {number} max - The maximum value in the range.
+ * @returns {number} A random integer between min and max, inclusive.
+ */
+export function randomIntFromRange(min: number, max: number): number {
   return Math.floor(randomUniform(min, max + 1));
 }
 
-export function randomUniform(min: number, max: number) {
+/**
+ * Generates a random floating-point number within a specified range, including the minimum value but excluding the maximum value.
+ * @param {number} min - The minimum value in the range.
+ * @param {number} max - The maximum value in the range.
+ * @returns {number} A random floating-point number between min and max, exclusive of max.
+ */
+export function randomUniform(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
