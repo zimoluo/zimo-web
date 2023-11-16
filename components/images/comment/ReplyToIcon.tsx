421ca7057d0ca128d32e9429122ba9d3a30747a0
zimoluo <zimo_luo@outlook.com>
2023-11-16 19:19:32 +0800

    use saturated for replyto

--- components/images/comment/ReplyToIcon.tsx
+++ components/images/comment/ReplyToIcon.tsx
@@ -16,7 +16,7 @@ export default function ReplyToIcon({ className = "", color }: ImageIconProps) {
       aria-label="Reply to"
     >
       <path
-        className={color ? "" : "fill-primary"}
+        className={color ? "" : "fill-saturated"}
         fill={color || undefined}
         d="M835.487 550.517 320.332 987.046c-25.104 21.274-45.454 11.842-45.454-21.063V58.017c0-32.905 20.35-42.335 45.454-21.063l515.155 436.529c25.104 21.272 25.104 55.762 0 77.034Z"
       />
