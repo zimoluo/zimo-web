f084f82148260f7c1132660051ae5b1b3976ca6e
zimoluo <zimo_luo@outlook.com>
2023-11-18 14:53:20 +0800

    aaaa

--- components/widgets/ArticleListLayout.tsx
+++ components/widgets/ArticleListLayout.tsx
@@ -20,7 +20,13 @@ export default function ArticleListLayout({
 }: Props) {
   return (
     <section className="my-20 px-8 md:px-36">
-      <h1 className="text-3xl font-bold mb-2 text-center">{title}</h1>
+      <h1
+        className={`text-3xl font-bold ${
+          subtitle ? "mb-2" : "mb-8"
+        } text-center`}
+      >
+        {title}
+      </h1>
       {subtitle && <h2 className="mb-8 text-center text-xl">{subtitle}</h2>}
       <FilterSearchProvider>
         <nav className="mb-4 flex items-center md:justify-end">
