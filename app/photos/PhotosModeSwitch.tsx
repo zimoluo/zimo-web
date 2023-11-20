4df4da290acb785ab89d57b0ce8d2f11206807d9
zimoluo <zimo_luo@outlook.com>
2023-11-20 16:07:37 +0800

    switch adjusted

--- app/photos/PhotosModeSwitch.tsx
+++ app/photos/PhotosModeSwitch.tsx
@@ -9,14 +9,14 @@ export default function PhotosModeSwitch() {
   const { settings, updateSettings } = useSettings();
 
   return (
-    <section className="inline-flex items-center rounded-full px-4 py-1 md:px-4 md:py-2 bg-light bg-opacity-60 backdrop-blur-lg relative overflow-hidden border-0.8 border-soft">
+    <section className="inline-flex items-center rounded-full px-3 py-1 md:px-3.5 md:py-1.5 bg-light bg-opacity-60 backdrop-blur-lg relative overflow-hidden border-0.8 border-soft">
       <div
         className={`absolute top-1/2 bg-saturated w-12 md:w-14 rounded-full h-auto aspect-square transition-transform duration-300 ${
           settings.enableGallery ? modeSwitchStyle.off : modeSwitchStyle.on
         }`}
       />
       <button
-        className="ml-0.5 md:ml-1 my-0.5 mr-3 md:mr-4 relative group"
+        className="ml-0.5 md:ml-1 my-0.5 mr-3 md:mr-3.5 relative group"
         onClick={() => {
           if (settings.enableGallery) {
             updateSettings({ enableGallery: false });
@@ -25,17 +25,17 @@ export default function PhotosModeSwitch() {
       >
         <PhotosStackIcon
           isLight={true}
-          className="w-7 h-auto aspect-square transition-transform duration-300 ease-in-out group-hover:scale-110"
+          className="w-6 md:w-7 h-auto aspect-square transition-transform duration-300 ease-in-out group-hover:scale-110"
         />
         <PhotosStackIcon
           isLight={false}
-          className={`w-7 h-auto aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out group-hover:scale-110 ${
+          className={`w-6 md:w-7 h-auto aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out group-hover:scale-110 ${
             settings.enableGallery ? "opacity-100" : "opacity-0"
           }`}
         />
       </button>
       <button
-        className="relative group mr-0.5 md:mx-1 my-0.5"
+        className="relative group mr-0.5 md:mx-0.5 my-0.5"
         onClick={() => {
           if (!settings.enableGallery) {
             updateSettings({ enableGallery: true });
@@ -44,11 +44,11 @@ export default function PhotosModeSwitch() {
       >
         <GalleryModeIcon
           isLight={false}
-          className="w-7 h-auto aspect-square transition-transform duration-300 ease-in-out group-hover:scale-110"
+          className="w-6 md:w-7 h-auto aspect-square transition-transform duration-300 ease-in-out group-hover:scale-110"
         />
         <GalleryModeIcon
           isLight={true}
-          className={`w-7 h-auto aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out group-hover:scale-110 ${
+          className={`w-6 md:w-7 h-auto aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out group-hover:scale-110 ${
             settings.enableGallery ? "opacity-100" : "opacity-0"
           }`}
         />
