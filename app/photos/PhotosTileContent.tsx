ebe2675d6c60342700f3cdc99f02c6e912b67754
zimoluo <zimo_luo@outlook.com>
2023-11-20 13:31:48 +0800

    hover effect

--- app/photos/PhotosTileContent.tsx
+++ app/photos/PhotosTileContent.tsx
@@ -1,7 +1,10 @@
+import HeartIcon from "@/components/images/comment/likeButtons/HeartIcon";
+import PhotosStackIcon from "@/components/images/entries/PhotosStackIcon";
+import { getEntryLike } from "@/lib/dataLayer/server/commentManager";
 import { computeRandomMultiplier } from "@/lib/photos/aspectRatioCalculator";
 import Image from "next/image";
 
-export default function PhotosTileContent(entry: PhotosEntry) {
+export default async function PhotosTileContent(entry: PhotosEntry) {
   const randomMultiplier = computeRandomMultiplier(entry.images.url[0]);
   const [widthRatio, heightRatio] = entry.images.aspectRatio
     .split(":")
@@ -21,9 +24,27 @@ export default function PhotosTileContent(entry: PhotosEntry) {
         height={288 / computedAspectRatio}
       />
       <div className="absolute inset-0 bg-black bg-opacity-60 select-none transition-opacity duration-300 ease-out opacity-0 group-hover:opacity-90" />
-      <div className="flex items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-out opacity-0 group-hover:opacity-90">
-        <div className="sr-only opacity-0 pointer-events-none select-none">
-          {entry.title}
+      <div className="sr-only opacity-0 pointer-events-none select-none flex-none">
+        {entry.title}
+      </div>
+      <div className="flex flex-col justify-center items-start space-y-1 md:space-y-2 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-out opacity-0 group-hover:opacity-90">
+        <div className="flex justify-center items-center">
+          <PhotosStackIcon
+            className="aspect-square w-5 md:w-7 h-auto mr-1.5 md:mr-2.5"
+            color="#f5f5f5"
+          />
+          <div className="text-base md:text-xl text-neutral-100">
+            {entry.images.url.length}
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <HeartIcon
+            className="aspect-square w-5 md:w-7 h-auto mr-1.5 md:mr-2.5"
+            color="#f5f5f5"
+          />
+          <div className="text-base md:text-xl text-neutral-100">
+            {(await getEntryLike(`photos/likedBy/${entry.slug}.json`)).length}
+          </div>
         </div>
       </div>
     </>
