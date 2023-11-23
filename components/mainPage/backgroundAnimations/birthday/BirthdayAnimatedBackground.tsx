ac76a5db48e06d9c2b3a3d706bb581d9cb7de34f
zimoluo <zimo_luo@outlook.com>
2023-11-23 11:10:52 +0800

    added eighteen to reduced

--- components/mainPage/backgroundAnimations/birthday/BirthdayAnimatedBackground.tsx
+++ components/mainPage/backgroundAnimations/birthday/BirthdayAnimatedBackground.tsx
@@ -23,13 +23,25 @@ export default function BirthdayAnimatedBackground() {
       </div>
     </>
   ) : (
-    <Image
-      src="/theme/animated-background/birthday/static-balloon.svg"
-      height={300}
-      width={300}
-      alt="Static balloon"
-      aria-hidden="true"
-      className="object-cover object-center -z-20 fixed inset-0 w-screen h-screen pointer-events-none select-none"
-    />
+    <>
+      <Image
+        src="/theme/animated-background/birthday/static-balloon.svg"
+        height={300}
+        width={300}
+        alt="Static balloon"
+        aria-hidden="true"
+        className="object-cover object-center -z-30 fixed inset-0 w-screen h-screen pointer-events-none select-none"
+      />
+      <div className="object-contain object-center fixed -z-40 inset-0 flex items-center justify-center w-screen h-screen pointer-events-none select-none">
+        <Image
+          src="/theme/animated-background/birthday/eighteen.svg"
+          height={300}
+          width={300}
+          alt="Eighteen"
+          aria-hidden="true"
+          className={`object-contain object-center -z-20 ${birthdayStyle.eighteen} pointer-events-none select-none`}
+        />
+      </div>
+    </>
   );
 }
