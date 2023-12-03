50c13cac67133b15bfc7868650d4e559751b2b78
zimoluo <zimo_luo@outlook.com>
2023-12-03 21:30:07 +0800

    randomize bubble favicon id

--- components/images/displayFavicon/BubblesFavicon.tsx
+++ components/images/displayFavicon/BubblesFavicon.tsx
@@ -1,3 +1,5 @@
+"id random";
+
 export default function BubblesFavicon({
   className = "",
   height,
@@ -23,7 +25,7 @@ export default function BubblesFavicon({
     >
       <defs>
         <linearGradient
-          id="a"
+          id="a_0478bc169bfaa46d2656"
           x1={22.27}
           x2={1038.27}
           y1={530.27}
@@ -34,7 +36,7 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <linearGradient
-          id="d"
+          id="d_2f91084a2136bc597867"
           x1={6.456}
           x2={1054.08}
           y1={692.102}
@@ -46,7 +48,7 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <linearGradient
-          id="e"
+          id="e_f9314e44434a59dc37eb"
           x1={6.456}
           x2={1054.08}
           y1={692.102}
@@ -58,7 +60,7 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <linearGradient
-          id="h"
+          id="h_080f797596d14f6dc986"
           x1={5.314}
           x2={1052.94}
           y1={463.128}
@@ -70,7 +72,7 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <linearGradient
-          id="i"
+          id="i_650a3159dd564fe24834"
           x1={-276.928}
           x2={770.698}
           y1={988.178}
@@ -82,7 +84,7 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <linearGradient
-          id="j"
+          id="j_608b10425b34d8b6c84c"
           x1={261.96}
           x2={1309.59}
           y1={985.824}
@@ -94,30 +96,30 @@ export default function BubblesFavicon({
           <stop offset={1} stopColor="#bae4fd" />
         </linearGradient>
         <path
-          id="b"
+          id="b_f95f92743793e9e452aa"
           d="M22.27 530.27c0-280.561 227.439-508 508-508 280.561 0 508 227.439 508 508 0 280.561-227.439 508-508 508-280.561 0-508-227.439-508-508Z"
         />
         <path
-          id="f"
+          id="f_f2fbedadffa456366a92"
           d="M526.89-56.031c-219.954.959-407.267 138.433-482.3 331.777-19.167 73.108-30.761 149.294-33.456 227.768.468 6.099.886 12.215 1.559 18.255-.64.327-1.247.709-1.886 1.04-.09 5.673-.259 11.324-.259 17.02 0 378.406 202.39 709.271 504.649 891.061 1.04-.62 2.15-1.13 3.184-1.75 5.094 3.03 10.387 5.71 15.592 8.57 308.563-180.26 516.017-514.735 516.017-897.881 0-1.24-.06-2.465-.07-3.702-1.93-1.155-3.76-2.445-5.71-3.575 2.25-17.002 3.43-34.321 4.03-51.777-3.53-62.833-12.57-124.171-26.77-183.525C951.81 91.061 756.488-57.032 526.89-56.031Z"
         />
       </defs>
       <g>
         <g>
           <use
-            xlinkHref="#b"
-            fill="url(#a)"
+            xlinkHref="#b_f95f92743793e9e452aa"
+            fill="url(#a_0478bc169bfaa46d2656)"
             stroke="#0369a1"
             strokeLinecap="butt"
             strokeLinejoin="miter"
             strokeWidth={40}
           />
-          <clipPath id="c" clipRule="nonzero">
-            <use xlinkHref="#b" />
+          <clipPath id="c_35c103596d34f9f7c271" clipRule="nonzero">
+            <use xlinkHref="#b_f95f92743793e9e452aa" />
           </clipPath>
-          <g clipPath="url(#c)">
+          <g clipPath="url(#c_35c103596d34f9f7c271)">
             <path
-              fill="url(#d)"
+              fill="url(#d_2f91084a2136bc597867)"
               stroke="#0369a1"
               strokeLinecap="butt"
               strokeLinejoin="miter"
@@ -127,33 +129,33 @@ export default function BubblesFavicon({
             />
             <g>
               <use
-                xlinkHref="#f"
-                fill="url(#e)"
+                xlinkHref="#f_f2fbedadffa456366a92"
+                fill="url(#e_f9314e44434a59dc37eb)"
                 stroke="#0369a1"
                 strokeLinecap="butt"
                 strokeLinejoin="miter"
                 strokeWidth={40}
               />
-              <clipPath id="g" clipRule="nonzero">
-                <use xlinkHref="#f" />
+              <clipPath id="g_a02ba3c91424c4d1e8e3" clipRule="nonzero">
+                <use xlinkHref="#f_f2fbedadffa456366a92" />
               </clipPath>
               <g
                 stroke="#0369a1"
                 strokeLinecap="butt"
                 strokeLinejoin="miter"
                 strokeWidth={40}
-                clipPath="url(#g)"
+                clipPath="url(#g_a02ba3c91424c4d1e8e3)"
               >
                 <path
-                  fill="url(#h)"
+                  fill="url(#h_080f797596d14f6dc986)"
                   d="M9.42 465.917C8.17 178.886 239.84-54.813 526.872-56.063c287.031-1.251 520.728 230.42 521.978 517.451 1.25 287.031-230.416 520.729-517.448 521.981-287.032 1.25-520.73-230.42-521.982-517.452Z"
                 />
                 <path
-                  fill="url(#i)"
+                  fill="url(#i_650a3159dd564fe24834)"
                   d="M-270.617 986.864c-1.251-287.032 230.42-520.73 517.451-521.981 287.031-1.251 520.731 230.42 521.981 517.451 1.252 287.036-230.421 520.736-517.452 521.986-287.03 1.25-520.729-230.43-521.98-517.456Z"
                 />
                 <path
-                  fill="url(#j)"
+                  fill="url(#j_608b10425b34d8b6c84c)"
                   d="M264.061 984.533c-1.25-287.031 230.421-520.729 517.452-521.981 287.027-1.25 520.737 230.421 521.987 517.452 1.25 287.036-230.42 520.726-517.456 521.976-287.033 1.26-520.731-230.42-521.983-517.447Z"
                 />
               </g>
