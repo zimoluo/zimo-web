7f589343d9075d8ef82f79f4c30d169e82637c4d
zimoluo <zimo_luo@outlook.com>
2023-12-24 18:47:11 +0800

    dependency

--- app/christmas-tree/ChristmasTreePlacer.tsx
+++ app/christmas-tree/ChristmasTreePlacer.tsx
@@ -78,7 +78,7 @@ export default function ChristmasTreePlacer() {
         }, 0);
       }
     },
-    [hasConfirmWindow]
+    [hasConfirmWindow, treeContainerRef]
   );
 
   const onRelease = useCallback(() => {
