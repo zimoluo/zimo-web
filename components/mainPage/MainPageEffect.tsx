2dae796ebe889aa0d8538a709e379108bde80b45
zimoluo <zimo_luo@outlook.com>
2024-01-05 15:52:48 +0800

    reintroduced seasonal effect. fully functional this time

--- components/mainPage/MainPageEffect.tsx
+++ components/mainPage/MainPageEffect.tsx
@@ -3,11 +3,18 @@
 import { ReactNode, useEffect } from "react";
 import { useUser } from "../contexts/UserContext";
 import { useClientSideFlag } from "@/lib/clientLogicHooks";
-import { isHalloweenSeason } from "@/lib/seasonUtil";
+import {
+  isBirthday,
+  isChristmas,
+  isHalloweenDay,
+  isHalloweenSeason,
+} from "@/lib/seasonUtil";
 import { parseStoredSettings, useSettings } from "../contexts/SettingsContext";
 import { restoreClientUser } from "@/lib/dataLayer/client/accountStateCommunicator";
 import HalloweenPulse from "./special/HalloweenPulse";
 import ToastDisplay from "./ToastDisplay";
+import { defaultSettings } from "@/lib/constants/defaultSettings";
+import _ from "lodash";
 
 interface Props {
   children?: ReactNode;
@@ -20,14 +27,14 @@ export default function MainPageEffect({ children }: Props) {
   const { updateSettings, settings } = useSettings();
 
   useEffect(() => {
-    async function downloadUserInfo() {
+    async function downloadUserInfo(): Promise<SettingsState> {
       const savedRawSettings = localStorage.getItem("websiteSettings");
       const loadedSettings = parseStoredSettings(savedRawSettings || "") || {};
 
       updateSettings(loadedSettings, false);
 
       if (user !== null) {
-        return;
+        return loadedSettings;
       }
 
       try {
@@ -41,7 +48,7 @@ export default function MainPageEffect({ children }: Props) {
 
         if (!restoredUserInfo.exists) {
           console.log("No user session found.");
-          return;
+          return loadedSettings;
         }
 
         const { integratedUser, downloadedSettings } = restoredUserInfo;
@@ -49,12 +56,76 @@ export default function MainPageEffect({ children }: Props) {
         if (downloadedSettings !== null) {
           updateSettings(downloadedSettings, false);
         }
+        return downloadedSettings || loadedSettings;
       } catch (error) {
         console.error("Error in restoring user session:", error);
+      } finally {
+        return loadedSettings;
       }
     }
 
-    downloadUserInfo();
+    downloadUserInfo().then((preparedSettings) => {
+      if (
+        _.isEqual(preparedSettings.pageTheme, defaultSettings.pageTheme) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        updateSettings(
+          {
+            ...preparedSettings,
+            pageTheme: {
+              home: "plainDark",
+              blog: "plainDark",
+              photos: "plainDark",
+              projects: "plainDark",
+              about: "plainDark",
+              management: "plainDark",
+            },
+          },
+          false
+        );
+      }
+
+      if (isHalloweenDay()) {
+        updateSettings(
+          { ...preparedSettings, enableHalloweenEffect: true },
+          false
+        );
+      }
+
+      if (isBirthday()) {
+        updateSettings(
+          {
+            ...preparedSettings,
+            pageTheme: {
+              home: "birthday",
+              blog: "birthday",
+              photos: "birthday",
+              projects: "birthday",
+              about: "birthday",
+              management: "birthday",
+            },
+          },
+          false
+        );
+      }
+
+      if (isChristmas()) {
+        updateSettings(
+          {
+            ...preparedSettings,
+            pageTheme: {
+              home: "christmas",
+              blog: "christmas",
+              photos: "christmas",
+              projects: "christmas",
+              about: "christmas",
+              management: "christmas",
+            },
+          },
+          false
+        );
+      }
+    });
   }, []);
 
   return (
