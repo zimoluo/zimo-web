78cb4f602474b45d2b24a59bb87a331ac43eeeee
zimoluo <zimo_luo@outlook.com>
2024-01-15 14:07:44 +0800

    make title normal again lol

--- app/HomeSecretText.tsx
+++ app/HomeSecretText.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useToast } from "@/components/contexts/ToastContext";
 import { useEffect, useState } from "react";
 
 export default function HomeSecretText() {
@@ -12,43 +11,5 @@ export default function HomeSecretText() {
     }
   }, []);
 
-  const { appendToast } = useToast();
-
-  return (
-    <>
-      <button
-        className="shadow-lg rounded-xl bg-widget-70 backdrop-blur-lg px-4 py-4 mb-2"
-        onClick={() => {
-          appendToast({
-            title: "Zimo Web",
-            description: "1st!",
-          });
-        }}
-      >
-        Add some toast
-      </button>
-      <button
-        className="shadow-lg rounded-xl bg-widget-70 backdrop-blur-lg px-4 py-4 mb-2"
-        onClick={() => {
-          appendToast({
-            title: " Web",
-            description: "2nd!",
-          });
-        }}
-      >
-        Add aawawawa
-      </button>
-      <button
-        className="shadow-lg rounded-xl bg-widget-70 backdrop-blur-lg px-4 py-4 mb-2"
-        onClick={() => {
-          appendToast({
-            title: "Zimawawawawawawawawawao Web",
-            description: "3rd!",
-          });
-        }}
-      >
-        muda
-      </button>
-    </>
-  );
+  return titleName;
 }
