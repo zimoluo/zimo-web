7fbe3c035260d3976ccceec22b6edd63ab5173a2
zimoluo <zimo_luo@outlook.com>
2024-01-15 15:58:28 +0800

    failed to fix so random stuff

--- components/contexts/SettingsContext.tsx
+++ components/contexts/SettingsContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { useUser } from "./UserContext";
 import { defaultSettings } from "@/lib/constants/defaultSettings";
 import { syncUpUserSettings } from "@/lib/dataLayer/client/accountStateCommunicator";
