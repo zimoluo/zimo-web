8a330c4e4b6af38b99b6d278ce6dbd1308e2bc2d
zimoluo <zimo_luo@outlook.com>
2024-04-05 14:47:15 +0800

    Revert "changed slider thingy color"
    This reverts commit b609e6a0f72798a6ad8b077bfb485c0e4062b12b.

--- components/mainPage/menu/settings/SettingsSlider.tsx
+++ components/mainPage/menu/settings/SettingsSlider.tsx
@@ -167,7 +167,7 @@ export default function SettingsSlider({
             className={`border-soft border-0.6 shadow-md w-2.5 h-6 rounded-full transition-all ease-in-out -translate-x-1 -translate-y-2.25 touch-none ${
               isDragging
                 ? "cursor-grabbing scale-150 bg-highlight"
-                : `cursor-grab scale-135 bg-pastel`
+                : `cursor-grab scale-135 bg-light`
             }`}
             draggable={true}
             onDragStart={dragStartHandler}
