85e20af55564bfdb5102dd3cfa47d1a6fc36b7d4
zimoluo <zimo_luo@outlook.com>
2024-04-16 22:42:43 +0800

    made project tile name always visible on mobile

--- app/projects/ProjectsTileContent.tsx
+++ app/projects/ProjectsTileContent.tsx
@@ -21,7 +21,7 @@ export default function ProjectsTileContent({
         className="h-12 md:h-20 w-auto transition-transform ease-in duration-200 group-hover:scale-110 opacity-90"
         alt={title}
       />
-      <p className="absolute top-28 md:top-40 left-1/2 -translate-x-1/2 w-44 text-center font-bold text-saturated transition-all text-sm md:text-base ease-in duration-200 opacity-0 group-hover:opacity-60 group-hover:top-26 md:group-hover:top-38">
+      <p className="absolute top-26 md:top-40 left-1/2 -translate-x-1/2 w-44 text-center font-bold text-saturated transition-all text-sm md:text-base ease-in duration-200 opacity-60 md:opacity-0 md:group-hover:opacity-60 md:group-hover:top-38">
         {title}
       </p>
     </>
