8f9bdb1e5843b43915f32ff99496eba49fd8850a
zimoluo <zimo_luo@outlook.com>
2024-04-19 18:43:33 +0800

    first of many styling issues?

--- components/widgets/ReadingLayout.tsx
+++ components/widgets/ReadingLayout.tsx
@@ -9,7 +9,7 @@ export default function ReadingLayout({ children }: Props) {
   return (
     <div className="flex justify-center items-center">
       <article
-        className={`relative mt-16 md:mt-20 md:mb-20 md:mx-8 px-6 w-full bg-widget-90 ${readingLayoutStyle.sizing} md:px-14 pb-12 pt-16 md:rounded-3xl md:shadow-xl`}
+        className={`relative mt-16 md:mt-20 md:mb-20 md:mx-8 px-6 bg-widget-90 ${readingLayoutStyle.sizing} md:px-14 pb-12 pt-16 md:rounded-3xl md:shadow-xl`}
       >
         {children}
       </article>
