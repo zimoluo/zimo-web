4963d08c24d14abc05c9ddc483e745e409151ed3
zimoluo <zimo_luo@outlook.com>
2024-04-22 18:16:55 +0800

    first steps lol

---
+++ app/design/theme-maker/ColorEditor.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useSettings } from "@/components/contexts/SettingsContext";
+import ColorEditorTypingArea from "./ColorEditorTypingArea";
+
+export default function ColorEditor() {
+  const { settings, updateSettings } = useSettings();
+
+  const updateColorScheme = (
+    entry: "primary" | "saturated" | "middle" | "soft" | "pastel" | "light",
+    index: number,
+    content: ColorSchemeData
+  ) => {
+    let themeData = [...settings.customThemeData];
+    themeData[index].palette[entry] = content;
+
+    updateSettings({ customThemeData: themeData });
+  };
+
+  const entries = ["primary", "saturated", "middle", "soft", "pastel", "light"];
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {entries.map((entry) => (
+        <ColorEditorTypingArea
+          key={entry}
+          setEntry={(colorData) =>
+            updateColorScheme(
+              entry as
+                | "primary"
+                | "saturated"
+                | "middle"
+                | "soft"
+                | "pastel"
+                | "light",
+              0,
+              colorData.split(",").map((num) => parseInt(num.trim())) as [
+                number,
+                number,
+                number
+              ]
+            )
+          }
+          entry={JSON.stringify(
+            settings.customThemeData[0].palette[
+              entry as
+                | "primary"
+                | "saturated"
+                | "middle"
+                | "soft"
+                | "pastel"
+                | "light"
+            ]
+          ).replace(/^\[|\]$/g, "")}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ app/design/theme-maker/layout.tsx
@@ -0,0 +1,9 @@
+import ColorEditor from "./ColorEditor";
+
+export default function ThemeMakerLayout() {
+  return (
+    <div className="my-20 flex items-center justify-center">
+      <ColorEditor />
+    </div>
+  );
+}
