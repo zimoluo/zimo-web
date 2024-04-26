0d56822971da264b2134a1a5ebf584b63100b872
zimoluo <zimo_luo@outlook.com>
2024-04-26 12:51:33 +0800

    marina favicon back lol

--- app/design/FaviconsGallery.tsx
+++ app/design/FaviconsGallery.tsx
@@ -3,24 +3,30 @@ import paletteStyle from "./palette.module.css";
 import GoldFavicon from "@/components/assets/displayFavicon/GoldFavicon";
 import SkyFavicon from "@/components/assets/displayFavicon/SkyFavicon";
 import GlitterFavicon from "@/components/assets/displayFavicon/GlitterFavicon";
-import PhotosFavicon from "@/components/assets/displayFavicon/PhotosFavicon";
 import BirthdayFavicon from "@/components/assets/displayFavicon/BirthdayFavicon";
+import { generateInlineStyleObject } from "@/lib/colorPaletteParser";
+import AdaptiveFavicon from "@/components/assets/displayFavicon/AdaptiveFavicon";
+import marinaConfig from "@/components/theme/config/marina";
 
 interface Props {
   className?: string;
 }
 
 export default function FaviconsGallery({ className = "" }: Props) {
+  const faviconClass = "w-12 md:w-14";
+
   return (
     <div
       className={`${paletteStyle.favicons} items-center justify-center ${className}`}
     >
-      <GenericFavicon className="w-12 md:w-14" />
-      <GoldFavicon className="w-12 md:w-14" />
-      <PhotosFavicon className="w-12 md:w-14" />
-      <SkyFavicon className="w-12 md:w-14" />
-      <GlitterFavicon className="w-12 md:w-14" />
-      <BirthdayFavicon className="w-12 md:w-14" />
+      <GenericFavicon className={faviconClass} />
+      <GoldFavicon className={faviconClass} />
+      <div style={generateInlineStyleObject(marinaConfig.palette)}>
+        <AdaptiveFavicon className={faviconClass} />
+      </div>
+      <SkyFavicon className={faviconClass} />
+      <GlitterFavicon className={faviconClass} />
+      <BirthdayFavicon className={faviconClass} />
     </div>
   );
 }
