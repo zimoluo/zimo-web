feae481f7c44b7000b37db959c59dfad045ab044
zimoluo <zimo_luo@outlook.com>
2024-04-26 12:26:48 +0800

    remove the code that fetches the theme

--- lib/dataLayer/server/themeServerManager.ts
+++ lib/dataLayer/server/themeServerManager.ts
@@ -1,15 +1,6 @@
 import "server-only";
 import { awsBucket, awsBucketRegion } from "@/lib/constants/awsConfig";
-import {
-  GetObjectCommand,
-  PutObjectCommandInput,
-  S3Client,
-} from "@aws-sdk/client-s3";
-import * as zlib from "zlib";
-import { Readable } from "stream";
-import { pipeline } from "stream/promises";
-import { cache } from "react";
-import { generateInlineStyleObject } from "@/lib/colorPaletteParser";
+import { PutObjectCommandInput, S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 
 const awsKeyId = process.env.ZIMO_WEB_AWS_KEY_ID;
@@ -30,49 +21,6 @@ const s3 = new S3Client({
     secretAccessKey: awsSecretKey,
   },
 });
-
-async function getRawColorPaletteData(
-  name: string
-): Promise<{ [key: string]: any }> {
-  const directory = "theme/colorPalette";
-  const params = {
-    Bucket: awsBucket,
-    Key: `${directory}/${name}.json`,
-  };
-  const command = new GetObjectCommand(params);
-  const s3Object = await s3.send(command);
-
-  if (!s3Object.Body) {
-    throw new Error("Failed to fetch color palette data from S3");
-  }
-
-  let fileContents = "";
-  const isGzipped = s3Object.ContentEncoding === "gzip";
-
-  await pipeline(
-    s3Object.Body as Readable,
-    isGzipped ? zlib.createGunzip() : (source) => source,
-    async function* (source) {
-      for await (const chunk of source) {
-        fileContents += chunk.toString("utf-8");
-      }
-    }
-  );
-
-  const data = JSON.parse(fileContents);
-  return data;
-}
-
-const fetchRawColorPaletteData = cache(async (name: string) => {
-  return await getRawColorPaletteData(name);
-});
-
-export async function getColorPaletteStyle(name: string) {
-  const rawData = await fetchRawColorPaletteData(name);
-  const styleObject = generateInlineStyleObject(rawData as RawColorPaletteData);
-
-  return styleObject;
-}
 
 const contentTypeMap: Record<AllowedImageFormat, string> = {
   jpeg: "image/jpeg",
