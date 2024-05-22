2bcf060fcfe33675dbe68c513ae3944cba532552
zimoluo <zimo_luo@outlook.com>
2024-05-22 16:01:46 +0800

    loading indicator

--- app/design/theme-maker/ImageUploadButton.tsx
+++ app/design/theme-maker/ImageUploadButton.tsx
@@ -2,7 +2,7 @@
 
 import PhotoIcon from "@/components/assets/entries/PhotoIcon";
 import { useToast } from "@/components/contexts/ToastContext";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { rgb, hex } from "color-convert";
 import {
   generateShadeMap,
@@ -103,6 +103,7 @@ const generateThemeConfig = (colorArray: ColorTriplet): ThemeDataConfig => {
 
 export default function ImageUploadButton({ insertProfile }: Props) {
   const { appendToast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const uploadImageInputRef = useRef<HTMLInputElement | null>(null);
 
   const uploadButtonClick = () => {
@@ -116,6 +117,10 @@ export default function ImageUploadButton({ insertProfile }: Props) {
   const handleImageUpload = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
+    if (isLoading) {
+      return;
+    }
+
     const file = event.target.files?.[0];
 
     if (!file) {
@@ -141,6 +146,8 @@ export default function ImageUploadButton({ insertProfile }: Props) {
     }
 
     try {
+      setIsLoading(true);
+
       const formData = new FormData();
       formData.append("file", file);
 
@@ -156,6 +163,7 @@ export default function ImageUploadButton({ insertProfile }: Props) {
           title: "Zimo Web",
           description: "Failed to analyze image.",
         });
+        setIsLoading(false);
         return;
       }
 
@@ -178,6 +186,7 @@ export default function ImageUploadButton({ insertProfile }: Props) {
       });
       console.log("Error uploading theme image file:", error);
     } finally {
+      setIsLoading(false);
       event.target.value = "";
     }
   };
@@ -186,6 +195,7 @@ export default function ImageUploadButton({ insertProfile }: Props) {
     <button
       onClick={uploadButtonClick}
       className="transition-transform hover:scale-110 duration-300 ease-in-out w-7 h-auto aspect-square"
+      disabled={isLoading}
     >
       <input
         type="file"
@@ -194,7 +204,11 @@ export default function ImageUploadButton({ insertProfile }: Props) {
         accept=".jpeg, .jpg, .png, .webp"
         className="w-0 h-0 m-0 p-0 border-none border-0 absolute opacity-0"
       />
-      <PhotoIcon className="w-full h-auto aspect-square" />
+      <PhotoIcon
+        className={`w-full h-auto aspect-square transition-opacity duration-300 ease-out ${
+          isLoading ? "opacity-50" : "opacity-100"
+        }`}
+      />
     </button>
   );
 }
