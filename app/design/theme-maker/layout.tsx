06e7b31c71c9aef2a0532c8fbaad50a9ce9471e8
zimoluo <zimo_luo@outlook.com>
2024-05-25 22:05:53 +0800

    update some metadata stuff

--- app/design/theme-maker/layout.tsx
+++ app/design/theme-maker/layout.tsx
@@ -1,5 +1,14 @@
 import ReadingBlur from "@/components/widgets/ReadingBlur";
 import ThemeMakerWindow from "./ThemeMakerWindow";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Theme Maker - Zimo Web",
+  description:
+    "Customize the appearance and the aesthetics of Zimo Web using theme profiles.",
+  keywords:
+    "Zimo Web, Zimo Luo, Color, Personal Website, Color Palette, Palette, Theme, Design, Editor, Web app, Theme Editor, Theme Maker, Interactive, Responsive, Online editor",
+};
 
 export default function ThemeMakerLayout() {
   return (
