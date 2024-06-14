ee42d0b769fab505209dfa57fdb8c46496c19527
zimoluo <zimo_luo@outlook.com>
2024-06-14 22:08:43 +0800

    a slight text shadow

--- components/mainPage/HeaderText.tsx
+++ components/mainPage/HeaderText.tsx
@@ -16,7 +16,7 @@ export default function HeaderText({
 }: Props) {
   return (
     <header
-      className={`${headerStyle.title} flex items-center justify-center px-12 ${className}`}
+      className={`${headerStyle.title} ${headerStyle.shadow} flex items-center justify-center px-12 ${className}`}
     >
       <h1 className="text-left font-bold text-5xl md:text-6xl">
         {title}
