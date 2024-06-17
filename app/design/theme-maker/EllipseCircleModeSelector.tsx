41577b29a5786d3466babcfaef8e1fc0c2a0d5f2
zimoluo <zimo_luo@outlook.com>
2024-06-17 16:37:37 +0800

    tweaked circle animation

--- app/design/theme-maker/EllipseCircleModeSelector.tsx
+++ app/design/theme-maker/EllipseCircleModeSelector.tsx
@@ -7,21 +7,18 @@ export default function EllipseCircleModeSelector() {
   const { selectedLayer, updateGradientProperty } = useGradientData();
   const isCircle = !!selectedLayer.isCircle;
 
-  const [rx, setRx] = useState(isCircle ? 370 : 440);
-  const [ry, setRy] = useState(isCircle ? 370 : 272);
+  const [rx, setRx] = useState(isCircle ? 347 : 440);
+  const [ry, setRy] = useState(isCircle ? 347 : 272);
 
   const handleClick = () => {
-    setRx(isCircle ? 440 : 370);
-    setRy(isCircle ? 272 : 370);
+    setRx(isCircle ? 440 : 347);
+    setRy(isCircle ? 272 : 347);
     updateGradientProperty("isCircle", !isCircle);
   };
 
   return (
     <div className="h-10 w-10 p-2 shrink-0 rounded-lg bg-pastel bg-opacity-80 shadow-sm flex flex-col gap-2">
-      <button
-        className="w-full h-auto aspect-square transition-transform duration-150 ease-out hover:scale-110"
-        onClick={handleClick}
-      >
+      <button className="w-full h-auto aspect-square" onClick={handleClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
