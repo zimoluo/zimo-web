be5be380ca69dc9d0660951f8881188af64596a0
zimoluo <zimo_luo@outlook.com>
2024-06-18 09:42:22 +0800

    dropdown menu improvements

--- app/design/theme-maker/EditorDropdownMenu.tsx
+++ app/design/theme-maker/EditorDropdownMenu.tsx
@@ -2,7 +2,6 @@
 
 import CircledEllipsisIcon from "@/components/assets/entries/CircledEllipsisIcon";
 import { Fragment, useEffect, useRef, useState } from "react";
-import DropdownMenuWrapper from "./DropdownMenuWrapper";
 
 interface Props<T> {
   optionsList: T[];
@@ -20,6 +19,8 @@ export default function EditorDropdownMenu<T>({
   fallbackName = "Unknown",
 }: Props<T>) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [hasSelected, setHasSelected] = useState(false);
+  const [previousIndex, setPreviousIndex] = useState(0);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const expandMenu = () => {
@@ -28,6 +29,7 @@ export default function EditorDropdownMenu<T>({
     }
 
     setIsExpanded(true);
+    setHasSelected(false);
   };
 
   useEffect(() => {
@@ -72,48 +74,47 @@ export default function EditorDropdownMenu<T>({
       </p>
       <button
         className={`${
-          isExpanded ? "pointer-events-none" : ""
+          isExpanded ? "pointer-events-none select-none" : ""
         } transition-transform duration-300 ease-out hover:scale-110 shrink-0`}
         onClick={expandMenu}
       >
         <CircledEllipsisIcon className="h-5 w-auto aspect-square" />
       </button>
-      {isExpanded && (
-        <DropdownMenuWrapper>
-          <div
-            className={`absolute z-5 w-full left-0 overflow-hidden rounded-xl cursor-auto shadow-lg top-0 ${
-              isExpanded ? "" : "pointer-events-none select-none"
-            }`}
-            style={{
-              maxHeight: isExpanded ? "max-content" : "2.5rem",
-              transform: `translate(0%, calc(${
-                currentValueIndex * -1
-              } * (2.5rem + 1px)))`,
-            }}
-          >
-            <div className="w-full flex-col flex gap-2 px-3 py-2 justify-center items-start bg-pastel">
-              {optionsList.map((option, index) => {
-                return (
-                  <Fragment key={index}>
-                    {index !== 0 && (
-                      <hr className="border-t w-full border-soft border-opacity-90" />
-                    )}
-                    <button
-                      onClick={() => {
-                        setValue(option);
-                        setIsExpanded(false);
-                      }}
-                      className="w-full text-left"
-                    >
-                      {namesList[index]}
-                    </button>
-                  </Fragment>
-                );
-              })}
-            </div>
-          </div>
-        </DropdownMenuWrapper>
-      )}
+      <div
+        className={`absolute z-5 w-full left-0 overflow-hidden max-h-max rounded-xl cursor-auto shadow-lg top-0 transition-opacity ease-out ${
+          isExpanded
+            ? "opacity-100 duration-150"
+            : "pointer-events-none select-none opacity-0 duration-300"
+        }`}
+        style={{
+          transform: `translate(0%, calc(${
+            (hasSelected ? previousIndex : currentValueIndex) * -1
+          } * (2.5rem + 1px)))`,
+        }}
+      >
+        <div className="w-full flex-col flex gap-2 px-3 py-2 justify-center items-start bg-pastel">
+          {optionsList.map((option, index) => {
+            return (
+              <Fragment key={index}>
+                {index !== 0 && (
+                  <hr className="border-t w-full border-soft border-opacity-90" />
+                )}
+                <button
+                  onClick={() => {
+                    setPreviousIndex(currentValueIndex);
+                    setValue(option);
+                    setIsExpanded(false);
+                    setHasSelected(true);
+                  }}
+                  className="w-full text-left"
+                >
+                  {namesList[index]}
+                </button>
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
