2ca81d0c908f62b68faa400dec317162fc19566a
zimoluo <zimo_luo@outlook.com>
2024-08-30 18:30:57 -0400

    change autumnal and cherry to when they were added

--- components/theme/config/autumnal.ts
+++ components/theme/config/autumnal.ts
@@ -9,105 +9,47 @@ const autumnalConfig: ThemeDataConfig = {
     page: [
       {
         type: "linear-gradient",
-        angle: 135,
         stops: [
-          {
-            color: [3, 29, 68],
-            opacity: 1,
-            at: 0,
-          },
-          {
-            color: [3, 29, 68],
-            opacity: 1,
-            at: 20,
-          },
-          {
-            color: [4, 57, 94],
-            opacity: 1,
-            at: 20.01,
-          },
-          {
-            color: [4, 57, 94],
-            opacity: 1,
-            at: 40,
-          },
-          {
-            color: [112, 162, 136],
-            opacity: 1,
-            at: 40.01,
-          },
-          {
-            color: [112, 162, 136],
-            opacity: 1,
-            at: 60,
-          },
-          {
-            color: [218, 183, 133],
-            opacity: 1,
-            at: 60.01,
-          },
-          {
-            color: [218, 183, 133],
-            opacity: 1,
-            at: 80,
-          },
-          {
-            color: [213, 137, 111],
-            opacity: 1,
-            at: 80.01,
-          },
-          {
-            color: [213, 137, 111],
-            opacity: 1,
-            at: 100,
-          },
+          { color: [3, 29, 68], opacity: 1, at: 0 },
+          { color: [3, 29, 68], opacity: 1, at: 20 },
+          { color: [4, 57, 94], opacity: 1, at: 20.01 },
+          { color: [4, 57, 94], opacity: 1, at: 40 },
+          { color: [112, 162, 136], opacity: 1, at: 40.01 },
+          { color: [112, 162, 136], opacity: 1, at: 60 },
+          { color: [218, 183, 133], opacity: 1, at: 60.01 },
+          { color: [218, 183, 133], opacity: 1, at: 80 },
+          { color: [213, 137, 111], opacity: 1, at: 80.01 },
+          { color: [213, 137, 111], opacity: 1, at: 100 },
         ],
+        linearGradientKeyword: true,
+        linearHorizontalOrientation: "right",
+        linearVerticalOrientation: "bottom",
       },
     ],
     pageMinimal: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
-          {
-            color: [4, 57, 94],
-            opacity: 1,
-            at: 15,
-          },
-          {
-            color: [4, 57, 94],
-            opacity: 1,
-            at: 85,
-          },
+          { color: [4, 57, 94], opacity: 1, at: 15 },
+          { color: [4, 57, 94], opacity: 1, at: 85 },
         ],
+        angle: 90,
       },
     ],
     widget: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
-          {
-            color: [21, 89, 138],
-            opacity: 1,
-            at: 15,
-          },
-          {
-            color: [21, 89, 138],
-            opacity: 1,
-            at: 85,
-          },
+          { color: [21, 89, 138], opacity: 1, at: 15 },
+          { color: [21, 89, 138], opacity: 1, at: 85 },
         ],
+        angle: 90,
       },
     ],
   },
   siteThemeColor: "#15598a",
-  favicon: {
-    mode: "backdrop",
-  },
-  misc: {
-    readingBlur: 0,
-  },
+  favicon: { mode: "backdrop" },
+  misc: { readingBlur: 0 },
 };
 
 export default autumnalConfig;
--- components/theme/config/cherry.ts
+++ components/theme/config/cherry.ts
@@ -9,106 +9,47 @@ const cherryConfig: ThemeDataConfig = {
     page: [
       {
         type: "linear-gradient",
-        angle: 45,
         stops: [
-          {
-            color: [207, 212, 197],
-            opacity: 1,
-            at: 0,
-          },
-          {
-            color: [207, 212, 197],
-            opacity: 1,
-            at: 20,
-          },
-          {
-            color: [238, 207, 212],
-            opacity: 1,
-            at: 20.01,
-          },
-          {
-            color: [238, 207, 212],
-            opacity: 1,
-            at: 40,
-          },
-          {
-            color: [239, 185, 203],
-            opacity: 1,
-            at: 40.01,
-          },
-          {
-            color: [239, 185, 203],
-            opacity: 1,
-            at: 60,
-          },
-          {
-            color: [230, 173, 236],
-            opacity: 1,
-            at: 60.01,
-          },
-          {
-            color: [230, 173, 236],
-            opacity: 1,
-            at: 80,
-          },
-          {
-            color: [194, 135, 232],
-            opacity: 1,
-            at: 80.01,
-          },
-          {
-            color: [194, 135, 232],
-            opacity: 1,
-            at: 100,
-          },
+          { color: [207, 212, 197], opacity: 1, at: 0 },
+          { color: [207, 212, 197], opacity: 1, at: 20 },
+          { color: [238, 207, 212], opacity: 1, at: 20.01 },
+          { color: [238, 207, 212], opacity: 1, at: 40 },
+          { color: [239, 185, 203], opacity: 1, at: 40.01 },
+          { color: [239, 185, 203], opacity: 1, at: 60 },
+          { color: [230, 173, 236], opacity: 1, at: 60.01 },
+          { color: [230, 173, 236], opacity: 1, at: 80 },
+          { color: [194, 135, 232], opacity: 1, at: 80.01 },
+          { color: [194, 135, 232], opacity: 1, at: 100 },
         ],
+        linearGradientKeyword: true,
+        linearHorizontalOrientation: "right",
+        linearVerticalOrientation: "top",
       },
     ],
     pageMinimal: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
-          {
-            color: [238, 207, 212],
-            opacity: 1,
-            at: 15,
-          },
-          {
-            color: [238, 207, 212],
-            opacity: 1,
-            at: 85,
-          },
+          { color: [238, 207, 212], opacity: 1, at: 15 },
+          { color: [238, 207, 212], opacity: 1, at: 85 },
         ],
+        angle: 90,
       },
     ],
     widget: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
-          {
-            color: [255, 235, 249],
-            opacity: 1.0,
-            isWidgetOpacity: true,
-            at: 10,
-          },
-          {
-            color: [255, 235, 249],
-            opacity: 1,
-            at: 60,
-          },
+          { color: [255, 235, 249], opacity: 1, isWidgetOpacity: true, at: 10 },
+          { color: [255, 235, 249], opacity: 1, at: 60 },
         ],
+        angle: 90,
       },
     ],
   },
   siteThemeColor: "#efb9cb",
-  favicon: {
-    mode: "backdrop",
-  },
-  misc: {
-    readingBlur: 0,
-  },
+  favicon: { mode: "backdrop" },
+  misc: { readingBlur: 0 },
 };
 
 export default cherryConfig;
