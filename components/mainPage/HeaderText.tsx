ff3e0e29d7e3e6a79512a2f145777b8622958798
zimoluo <zimo_luo@outlook.com>
2024-09-05 23:27:55 -0400

    some random option

--- components/mainPage/HeaderText.tsx
+++ components/mainPage/HeaderText.tsx
@@ -6,6 +6,7 @@ interface Props {
   subtitle: string | ReactNode;
   className?: string;
   children?: ReactNode;
+  isGradient?: boolean;
 }
 
 export default function HeaderText({
@@ -13,12 +14,17 @@ export default function HeaderText({
   subtitle,
   className = "",
   children,
+  isGradient = false,
 }: Props) {
   return (
     <header
       className={`${headerStyle.title} ${headerStyle.shadow} flex items-center justify-center px-12 md:px-16 ${className}`}
     >
-      <h1 className="text-left font-bold text-5xl md:text-6xl">
+      <h1
+        className={`text-left font-bold text-5xl md:text-6xl ${
+          isGradient ? headerStyle.gradient : ""
+        }`}
+      >
         {title}
         <div className="text-lg md:text-xl font-normal mt-4">{subtitle}</div>
         {children}
