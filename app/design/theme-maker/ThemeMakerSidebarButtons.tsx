2a5230cc29c5a0f016d826909076167901bfcbd7
zimoluo <zimo_luo@outlook.com>
2024-09-10 11:14:38 -0400

    always centered

--- app/design/theme-maker/ThemeMakerSidebarButtons.tsx
+++ app/design/theme-maker/ThemeMakerSidebarButtons.tsx
@@ -21,11 +21,13 @@ interface Props {
   alwaysHorizontal?: boolean;
   sidebarOptions?: SidebarButtonsOption[];
   noBackground?: boolean;
+  alwaysCentered?: boolean;
 }
 
 export default function ThemeMakerSidebarButtons({
   alwaysHorizontal = false,
   noBackground = false,
+  alwaysCentered = false,
   sidebarOptions = [
     "sidebar",
     "customTheme",
@@ -208,7 +210,9 @@ export default function ThemeMakerSidebarButtons({
         noBackground ? "bg-none bg-transparent" : "bg-light bg-opacity-80"
       } w-full ${alwaysHorizontal ? "" : "md:w-12"} h-12 ${
         alwaysHorizontal ? "" : "md:h-full"
-      } flex ${alwaysHorizontal ? "" : "md:flex-col"} items-center px-4 ${
+      } flex ${alwaysHorizontal ? "" : "md:flex-col"} items-center ${
+        alwaysCentered ? "justify-center" : ""
+      } px-4 ${
         alwaysHorizontal ? "" : "md:px-0 md:py-4"
       } gap-4 overflow-x-auto overflow-y-hidden ${
         alwaysHorizontal ? "" : "md:overflow-x-hidden md:overflow-y-auto"
