1e838b0fd455967dca81f09ac0d45bc296b866d5
zimoluo <zimo_luo@outlook.com>
2024-09-20 11:46:48 -0400

    interpolating

--- app/design/theme-maker/ThemeMakerWindowWrapper.tsx
+++ app/design/theme-maker/ThemeMakerWindowWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import windowStyle from "./window.module.css";
 import { useSettings } from "@/components/contexts/SettingsContext";
 
@@ -10,14 +10,25 @@ interface Props {
 
 export default function ThemeMakerWindowWrapper({ children }: Props) {
   const { settings } = useSettings();
+  const [isInterpolating, setIsInterpolating] = useState(false);
+  const [fullScreenBuffer, setFullScreenBuffer] = useState(false);
+
   const isFullscreen = settings.expandThemeMakerWindow;
+
+  useEffect(() => {
+    setIsInterpolating(true);
+    setFullScreenBuffer(isFullscreen);
+    setTimeout(() => {
+      setIsInterpolating(false);
+    }, 300);
+  }, [isFullscreen]);
 
   return (
     <div
-      className={`${windowStyle.transition} mt-16 ${
-        isFullscreen ? "md:mb-6" : "md:my-18 md:rounded-3xl md:shadow-xl"
+      className={`${isInterpolating ? windowStyle.transition : ""} mt-16 ${
+        fullScreenBuffer ? "md:mb-6" : "md:my-18 md:rounded-3xl md:shadow-xl"
       } ${
-        isFullscreen ? windowStyle.fullscreen : windowStyle.sizing
+        fullScreenBuffer ? windowStyle.fullscreen : windowStyle.sizing
       } bg-widget-80 md:bg-widget-40 md:backdrop-blur-2xl md:overflow-hidden`}
     >
       {children}
