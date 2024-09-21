025cc511c91b3eee8b7ac3f4ae7cb7d6525e47c9
zimoluo <zimo_luo@outlook.com>
2024-09-21 00:34:24 -0400

    no mobile

--- components/widgets/WindowManager.tsx
+++ components/widgets/WindowManager.tsx
@@ -2,10 +2,30 @@
 
 import { useWindow } from "../contexts/WindowContext";
 import WindowInstance from "./WindowInstance";
+import { useState, useEffect } from "react";
 
 export default function WindowManager() {
-  const { windows } = useWindow();
-  return (
+  const { windows, clearWindow } = useWindow();
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (window.innerWidth < 768) {
+        clearWindow();
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowWidth < 768 ? null : (
     <div className="fixed inset-0 w-screen h-screen z-[11] pointer-events-none select-none">
       {windows.map((windowData) => {
         return <WindowInstance data={windowData} key={windowData.uniqueId} />;
