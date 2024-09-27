690410cb5c70bfd03158ce83b2ece83467ce1ffc
zimoluo <zimo_luo@outlook.com>
2024-09-27 16:53:10 -0400

    16rem

--- components/window/blog/BlogWindowMenu.tsx
+++ components/window/blog/BlogWindowMenu.tsx
@@ -73,7 +73,7 @@ export default function BlogWindowMenu({ isMainPage = false }: Props) {
         <SearchCardColumn
           keywords={keywords}
           components={entries}
-          cardHeight="15rem"
+          cardHeight="16rem"
         />
       </div>
     </div>
