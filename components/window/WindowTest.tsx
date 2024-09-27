eabf020f9db848183a8c98bdd356c03427a8fd95
zimoluo <zimo_luo@outlook.com>
2024-09-27 18:34:44 -0400

    another one

--- components/window/WindowTest.tsx
+++ components/window/WindowTest.tsx
@@ -2,9 +2,6 @@
 
 import { useEffect } from "react";
 import { useWindow } from "../contexts/WindowContext";
-import ImageViewer from "../widgets/ImageViewer";
-import MusicPlayerCard from "../widgets/MusicPlayerCard";
-import FaviconViewer from "@/app/design/theme-maker/FaviconViewer";
 import BlogWindowFrame from "./blog/BlogWindowFrame";
 
 export default function WindowTest() {
@@ -12,39 +9,14 @@ export default function WindowTest() {
 
   useEffect(() => {
     appendWindow({
-      content: (
-        <ImageViewer
-          defaultDimension={false}
-          className="h-full w-auto"
-          aspectRatio="2.1:1"
-          url={[
-            "https://zimo-web-bucket.s3.us-east-2.amazonaws.com/blog/posts/infinity-update/images/gallery.png",
-            "https://zimo-web-bucket.s3.us-east-2.amazonaws.com/blog/posts/infinity-update/images/eep.png",
-            "https://zimo-web-bucket.s3.us-east-2.amazonaws.com/blog/posts/infinity-update/images/vibrant.png",
-          ]}
-        />
-      ),
-      contextKey: "test",
-      defaultHeight: "fit",
-      defaultWidth: 600,
-      minWidth: 300,
-      disableExpandToScreen: true,
-    });
-    appendWindow({
-      content: (
-        <MusicPlayerCard url="https://zimo-web-bucket.s3.us-east-2.amazonaws.com/blog/audio/at-the-speed-of-light.mp3" />
-      ),
-      contextKey: "test2",
-      defaultHeight: "fit",
-      defaultWidth: 600,
-      minWidth: 300,
-    });
-    appendWindow({
-      content: <FaviconViewer />,
-      contextKey: "test3",
-      defaultHeight: "fit",
-      defaultWidth: 600,
-      minWidth: 144,
+      content: <BlogWindowFrame />,
+      contextKey: "test5",
+      defaultHeight: 620,
+      defaultWidth: 460,
+      minWidth: 416,
+      minHeight: 420,
+      maxWidth: 960,
+      maxHeight: 760,
     });
     appendWindow({
       content: <BlogWindowFrame />,
