7f9851a2f2c0d4ace15bd4c78ece402478cf5b82
zimoluo <zimo_luo@outlook.com>
2024-09-29 21:59:18 -0400

    very minor change

--- components/mainPage/menu/MenuEntriesLayout.tsx
+++ components/mainPage/menu/MenuEntriesLayout.tsx
@@ -16,11 +16,11 @@ const menuNavigationItems = [
 export default function MenuEntriesLayout() {
   return (
     <div className="h-full w-full overflow-y-auto px-6 md:px-8 py-8">
-      <div className="rounded-full w-full bg-light bg-opacity-80 shadow-lg px-4 py-4 mt-6 mb-14 flex items-center">
+      <div className="rounded-full w-full bg-light bg-opacity-80 shadow-lg px-4 py-4 mt-8 mb-14 flex items-center">
         <MenuEntriesUser />
       </div>
 
-      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-4 my-6 text-lg md:text-xl grid grid-cols-1 gap-4">
+      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-4 my-8 text-lg md:text-xl grid grid-cols-1 gap-4">
         {menuNavigationItems.map((item, index) => (
           <MenuEntriesNavigation
             key={item}
@@ -30,11 +30,11 @@ export default function MenuEntriesLayout() {
         ))}
       </div>
 
-      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-4 my-6 text-lg md:text-xl grid grid-cols-1 gap-4">
+      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-4 my-8 text-lg md:text-xl grid grid-cols-1 gap-4">
         <MenuEntriesSettings />
       </div>
 
-      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-0 my-6 text-lg md:text-xl">
+      <div className="rounded-2xl w-full bg-light bg-opacity-80 shadow-lg px-6 py-0 my-8 text-lg md:text-xl">
         <MenuEntriesUtility />
       </div>
     </div>
