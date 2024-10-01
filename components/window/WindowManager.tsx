32809d9a5760f703e238b7d7eeddf318765fb499
zimoluo <zimo_luo@outlook.com>
2024-10-01 10:09:07 -0400

    just use css to block it

--- components/window/WindowManager.tsx
+++ components/window/WindowManager.tsx
@@ -2,31 +2,12 @@
 
 import { useWindow } from "../contexts/WindowContext";
 import WindowInstance from "./WindowInstance";
-import { useState, useEffect } from "react";
 
 export default function WindowManager() {
-  const { windows, clearWindow, windowOrder } = useWindow();
-  const [windowWidth, setWindowWidth] = useState(0);
+  const { windows, windowOrder } = useWindow();
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-      if (window.innerWidth < 768) {
-        clearWindow();
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return windowWidth < 768 ? null : (
-    <div className="fixed inset-0 w-screen h-screen z-[11] pointer-events-none">
+  return (
+    <div className="hidden md:block fixed inset-0 w-screen h-screen z-[11] pointer-events-none">
       {windows.map((windowData, index) => {
         return (
           <WindowInstance
