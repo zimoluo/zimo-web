12d783bc4681f7cb197e887ce524eba581ced01f
zimoluo <zimo_luo@outlook.com>
2024-10-03 12:46:43 -0400

    might be useful in the future

--- components/window/widget/WindowNotebook.tsx
+++ components/window/widget/WindowNotebook.tsx
@@ -3,15 +3,18 @@ import NotebookTopBar from "./NotebookTopBar";
 import NotebookMenu from "./NotebookMenu";
 import { NotebookProvider } from "@/components/contexts/NotebookContext";
 import NotebookGridWrapper from "./NotebookGridWrapper";
+import notebookStyle from "./notebook.module.css";
 
 export default function WindowNotebook() {
   return (
     <NotebookProvider>
-      <NotebookGridWrapper>
-        <NotebookTopBar />
-        <NotebookMenu />
-        <NotebookPage />
-      </NotebookGridWrapper>
+      <div className={`w-full h-full ${notebookStyle.container}`}>
+        <NotebookGridWrapper>
+          <NotebookTopBar />
+          <NotebookMenu />
+          <NotebookPage />
+        </NotebookGridWrapper>
+      </div>
     </NotebookProvider>
   );
 }
