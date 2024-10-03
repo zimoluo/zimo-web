168a79d402e5d6132d410d0d435fbd7953b108cf
zimoluo <zimo_luo@outlook.com>
2024-10-03 18:47:55 -0400

    metadata stuff

--- app/notebook/layout.tsx
+++ app/notebook/layout.tsx
@@ -1,10 +1,18 @@
 import WindowNotebook from "@/components/window/widget/WindowNotebook";
 import { ReactNode } from "react";
 import notebookPageStyle from "./notebook-page.module.css";
+import { Metadata } from "next";
 
 interface Props {
   children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Notebook - Zimo Web",
+  description: "Lightweight notebook built into Zimo Web.",
+  keywords:
+    "Zimo Web, Zimo Luo, Personal Website, Editor, Web app, Interactive, Responsive, Online editor, Notebook, Note taking, Note-taking, Productivity",
+};
 
 export default function NotebookLayout({ children }: Props) {
   return (
