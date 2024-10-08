4a2b7a456c0bdd504daff79512a605574e063449
zimoluo <zimo_luo@outlook.com>
2024-10-07 21:37:21 -0400

    that doesnt exist yet

--- lib/lightMarkUpProcessor.tsx
+++ lib/lightMarkUpProcessor.tsx
@@ -120,8 +120,7 @@ export const restoreDisplayText = (content: string): string => {
     .replace(/\*(.*?)\*/g, "$1")
     .replace(/_(.*?)_/g, "$1")
     .replace(/`(.*?)`/g, "$1")
-    .replace(/\|(.*?)\|/g, "$1")
-    .replace(/\^(.*?)\^/g, "$1");
+    .replace(/\|(.*?)\|/g, "$1");
 
   return cleanedContent;
 };
