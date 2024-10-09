803a66bf1871eaa35688ac7403441677587a81a8
zimoluo <zimo_luo@outlook.com>
2024-10-09 15:29:35 -0400

    formatting fix again

--- components/window/widget/CalculatorWidget.tsx
+++ components/window/widget/CalculatorWidget.tsx
@@ -206,18 +206,18 @@ export default function CalculatorWidget() {
         className={`w-full h-full p-4 ${calculatorStyle.containerGrid} font-tabular`}
       >
         <div
-          className={`bg-pastel bg-opacity-75 rounded-2xl p-3 ${calculatorStyle.displayGrid} items-end justify-end w-full overflow-hidden`}
+          className={`bg-pastel bg-opacity-75 rounded-2xl px-3 pt-1 pb-1 ${calculatorStyle.displayGrid} items-end justify-end w-full overflow-hidden`}
         >
           <div
             className="pointer-events-none select-none w-0 h-0 touch-none"
             aria-hidden="true"
           />
           {history && (
-            <p className="text-end leading-none overflow-x-auto overflow-y-hidden text-2xl mb-2 opacity-75 text-saturated">
+            <p className="text-end overflow-x-auto overflow-y-hidden text-2xl mb-0 opacity-75 text-saturated">
               {history}
             </p>
           )}
-          <p className="text-end text-3xl leading-none overflow-x-auto overflow-y-hidden">
+          <p className="text-end text-3xl leading-normal overflow-x-auto overflow-y-hidden">
             {expression.length ? getDisplayExpression() : "0"}
           </p>
         </div>
