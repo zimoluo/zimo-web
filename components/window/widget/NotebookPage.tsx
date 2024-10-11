594fcf48ceba276b50f4363c45c864389e140aff
zimoluo <zimo_luo@outlook.com>
2024-10-11 16:28:05 -0400

    using the new method

--- components/window/widget/NotebookPage.tsx
+++ components/window/widget/NotebookPage.tsx
@@ -2,13 +2,9 @@
 
 import { useNotebook } from "@/components/contexts/NotebookContext";
 import { useSettings } from "@/components/contexts/SettingsContext";
-import notebookStyle from "./notebook.module.css";
-import {
-  enrichTextContent,
-  restoreDisplayText,
-} from "@/lib/lightMarkUpProcessor";
-import { FormEvent, FormEventHandler, Fragment } from "react";
-import { applyStyleData, generateStyleData } from "@/lib/notebookUtil";
+import { enrichTextContent } from "@/lib/lightMarkUpProcessor";
+import { FormEvent } from "react";
+import ReactDOMServer from "react-dom/server";
 
 export default function NotebookPage() {
   const { settings, updateSettings } = useSettings();
@@ -16,7 +12,7 @@ export default function NotebookPage() {
   const isNotebookEmpty = notebookData.length === 0;
   const { setShouldScrollToTop, addNewNotebook } = useNotebook();
 
-  const handleChange = (e: FormEventHandler<HTMLDivElement>) => {
+  const handleChange = (e: FormEvent<HTMLDivElement>) => {
     if (isNotebookEmpty) {
       return;
     }
@@ -43,42 +39,27 @@ export default function NotebookPage() {
       return;
     }
 
-    console.log(
-      restoreDisplayText(
-        "this *is* a _test_. the\\* text *_will_* be |`_co*m**p*licated_`| and ~~{truly exciting}{https://www.zimoluo.me/}~~."
-      )
-    );
-    console.log(
-      JSON.stringify(
-        generateStyleData(
-          "this *is* a _test_. the\\* text *_will_* be |`_co*m**p*licated_`| and ~~{truly exciting}{https://www.zimoluo.me/}~~."
-        )
-      )
-    );
-    console.log(
-      applyStyleData(
-        "this is a test. the\\* text will be complicated and truly exciting.",
-        generateStyleData(
-          "this *is* a _test_. the\\* text *_will_* be |`_co*m**p*licated_`| and ~~{truly exciting}{https://www.zimoluo.me/}~~."
-        )
-      )
-    );
-
     addNewNotebook();
   };
 
   return (
     <div className="w-full h-full relative">
       <div
-        className="absolute top-0 left-0 w-full h-full text-lg p-4"
+        className="w-full h-full text-lg p-4 rounded-lg bg-light bg-opacity-80 shadow-lg"
         contentEditable={true}
+        role="textbox"
         onClick={handleClick}
         onInput={handleChange}
-      >
-        {isNotebookEmpty
-          ? ""
-          : enrichTextContent(notebookData[notebookIndex].content)}
-      </div>
+        dangerouslySetInnerHTML={{
+          __html: ReactDOMServer.renderToStaticMarkup(
+            <>
+              {isNotebookEmpty
+                ? ""
+                : enrichTextContent(notebookData[notebookIndex].content)}
+            </>
+          ),
+        }}
+      />
     </div>
   );
 }
