05147eb8838953a89408281f5efcf36de898a28d
zimoluo <zimo_luo@outlook.com>
2024-10-15 12:23:11 -0400

    slightly optimize windowicon

--- components/assets/entries/WindowIcon.tsx
+++ components/assets/entries/WindowIcon.tsx
@@ -7,38 +7,24 @@ export default function WindowIcon({ className = "", color }: ImageIconProps) {
       className={className}
       aria-label="Window widget"
     >
-      <rect
-        width={947}
-        height={586}
-        x={39}
-        y={199}
-        className={color ? "" : "stroke-primary"}
-        stroke={color || undefined}
-        strokeWidth={47}
-        rx={70}
-      />
-      <path
-        className={color ? "" : "stroke-primary"}
-        stroke={color || undefined}
-        strokeLinecap="round"
-        strokeWidth={44}
-        d="M336 853h397"
-      />
-      <path
-        className={color ? "" : "stroke-primary"}
-        stroke={color || undefined}
-        strokeLinecap="round"
-        strokeWidth={44}
-        d="M178 310h397M178 487h295M178 401h334"
-        opacity={0.5}
-      />
-      <path
-        className={color ? "" : "stroke-primary"}
-        stroke={color || undefined}
-        strokeLinecap="round"
-        strokeWidth={44}
-        d="M269 853h0"
-      />
+      <g className={color ? "" : "stroke-primary"} stroke={color || undefined}>
+        <rect
+          width={947}
+          height={586}
+          x={39}
+          y={199}
+          strokeWidth={47}
+          rx={70}
+        />
+        <path strokeLinecap="round" strokeWidth={44} d="M336 853h397" />
+        <path
+          strokeLinecap="round"
+          strokeWidth={44}
+          d="M178 310h397M178 487h295M178 401h334"
+          opacity={0.5}
+        />
+        <path strokeLinecap="round" strokeWidth={44} d="M269 853h0" />
+      </g>
     </svg>
   );
 }
