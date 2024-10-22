a87e15d9df7e5b23763a2ded8d959dc3924be77c
zimoluo <zimo_luo@outlook.com>
2024-10-21 20:40:37 -0400

    better ui

--- components/window/widget/SignalGeneratorWindow.tsx
+++ components/window/widget/SignalGeneratorWindow.tsx
@@ -6,9 +6,11 @@ import SignalGeneratorInterface from "./SignalGeneratorInterface";
 import ExpandMenuButton from "@/components/widgets/ExpandMenuButton";
 import NotificationStylePicker from "@/components/mainPage/menu/settings/NotificationStylePicker";
 import { EntryWindowProvider } from "@/components/contexts/EntryWindowContext";
+import { useSettings } from "@/components/contexts/SettingsContext";
 
 export default function SignalGeneratorWindow(preset: Partial<ToastEntry>) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { settings } = useSettings();
   const menuButtonRef = useRef<HTMLButtonElement>(null);
 
   return (
@@ -16,6 +18,12 @@ export default function SignalGeneratorWindow(preset: Partial<ToastEntry>) {
       <div className="w-full h-full relative">
         <SignalGeneratorInterface {...preset} />
       </div>
+      <div
+        className={`right-3 top-3 absolute h-8 w-8 bg-light rounded-md shadow-md pointer-events-none select-none touch-none transition-opacity duration-300 ease-out ${
+          settings.notificationStyle === "banner" ? "opacity-100" : "opacity-0"
+        }`}
+        aria-hidden="true"
+      />
       <EntryWindowProvider
         slug="placeholder"
         setSlug={() => {}}
