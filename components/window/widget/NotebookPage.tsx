af0a548e8699b17d4fea5598489e23bb2277926a
zimoluo <zimo_luo@outlook.com>
2024-11-03 17:58:44 -0500

    Revert "experimental debounce"
    This reverts commit 85e7a2a2288a7bcc19ef95ff5cc38f4fefdb211d.

--- components/window/widget/NotebookPage.tsx
+++ components/window/widget/NotebookPage.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/lib/lightMarkUpProcessor";
 import notebookStyle from "./notebook.module.css";
 import _ from "lodash";
-import { useLayoutEffect, useRef, useCallback, useEffect } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import {
   applyNotebookPageStyleData,
@@ -23,105 +23,6 @@ export default function NotebookPage() {
     useNotebook();
 
   const editorRef = useRef<HTMLDivElement>(null);
-
-  const caretPositionRef = useRef({
-    position: 0,
-    shouldRestore: false,
-    isEmptyLine: false,
-    lineIndex: 0,
-  });
-
-  const windowSelection = (() => {
-    try {
-      return window.getSelection()?.getRangeAt(0);
-    } catch (e) {
-      return null;
-    }
-  })();
-
-  const saveCaretPosition = () => {
-    const selection = window.getSelection();
-    if (!selection || selection.rangeCount === 0 || !editorRef.current) {
-      return;
-    }
-
-    const range = selection.getRangeAt(0);
-
-    caretPositionRef.current.isEmptyLine = range.startContainer.nodeType !== 3;
-
-    const nodeIndex = Array.from(editorRef.current.childNodes).indexOf(
-      range.startContainer as any
-    );
-
-    if (caretPositionRef.current.isEmptyLine) {
-      if (nodeIndex === -1) {
-        const startOffset = range.startOffset;
-
-        caretPositionRef.current.lineIndex = (startOffset - 1) / 2 + 1;
-      } else {
-        caretPositionRef.current.lineIndex = Math.floor(nodeIndex / 2) + 1;
-      }
-    } else {
-      const preCaretRange = range.cloneRange();
-      preCaretRange.selectNodeContents(editorRef.current);
-      preCaretRange.setEnd(range.endContainer, range.endOffset);
-
-      caretPositionRef.current.position = preCaretRange.toString().length;
-    }
-  };
-
-  const restoreCaretPosition = () => {
-    const selection = window.getSelection();
-    if (!selection || !editorRef.current) {
-      return;
-    }
-
-    const range = document.createRange();
-
-    if (caretPositionRef.current.isEmptyLine) {
-      const lineIndex = caretPositionRef.current.lineIndex;
-      const emptyLineNode = editorRef.current.childNodes[lineIndex * 2];
-
-      if (emptyLineNode) {
-        range.setStart(emptyLineNode, 0);
-        range.setEnd(emptyLineNode, 0);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        return;
-      }
-    } else {
-      let charIndex = 0;
-      const pos = caretPositionRef.current.position;
-      const nodeStack = [editorRef.current];
-      let node: any;
-      let foundPosition = false;
-
-      while (!foundPosition && (node = nodeStack.pop())) {
-        if (node.nodeType === 3) {
-          const nextCharIndex = charIndex + node.length;
-          if (pos >= charIndex && pos <= nextCharIndex) {
-            range.setStart(node, pos - charIndex);
-            range.setEnd(node, pos - charIndex);
-            foundPosition = true;
-          }
-          charIndex = nextCharIndex;
-        } else {
-          let i = node.childNodes.length;
-          while (i--) {
-            nodeStack.push(node.childNodes[i]);
-          }
-        }
-      }
-
-      if (!foundPosition) {
-        range.selectNodeContents(editorRef.current);
-        range.collapse(false);
-      }
-
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
-  };
 
   const updateText = () => {
     if (isNotebookEmpty) {
@@ -280,15 +181,6 @@ export default function NotebookPage() {
     });
   };
 
-  const debouncedHandleInput = useCallback(
-    _.debounce(() => {
-      saveCaretPosition();
-      caretPositionRef.current.shouldRestore = true;
-      updateText();
-    }, 400),
-    [updateText]
-  );
-
   const handleClick = () => {
     if (!isNotebookEmpty) {
       return;
@@ -312,19 +204,6 @@ export default function NotebookPage() {
     }
   }, [notebookData, notebookIndex]);
 
-  useLayoutEffect(() => {
-    if (caretPositionRef.current.shouldRestore) {
-      caretPositionRef.current.shouldRestore = false;
-      restoreCaretPosition();
-    }
-  }, [notebookData[notebookIndex], windowSelection]);
-
-  useEffect(() => {
-    return () => {
-      debouncedHandleInput.cancel();
-    };
-  }, [debouncedHandleInput]);
-
   return (
     <div className="w-full h-full overflow-y-auto overflow-x-hidden border-none border-transparent rounded-lg resize-none text-lg bg-light bg-opacity-80 shadow-lg">
       <div className="w-full h-full grid">
@@ -333,7 +212,6 @@ export default function NotebookPage() {
           contentEditable={true}
           ref={editorRef}
           onBlur={updateText}
-          onInput={debouncedHandleInput}
           onClick={handleClick}
         />
       </div>
