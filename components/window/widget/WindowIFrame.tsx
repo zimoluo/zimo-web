3f300ea89a22eeb19b6d138c807e8bb763720a26
zimoluo <zimo_luo@outlook.com>
2024-11-08 20:31:57 -0500

    rework window web

--- components/window/widget/WindowIFrame.tsx
+++ components/window/widget/WindowIFrame.tsx
@@ -4,19 +4,32 @@ import { useWindowAction } from "@/components/contexts/WindowActionContext";
 
 interface Props {
   url: string;
+  className?: string;
+  zoom?: number;
 }
 
-export default function WindowIFrame({ url }: Props) {
-  const { isWindowDragging, isWindowResizing } = useWindowAction();
+export default function WindowIFrame({ url, className = "", zoom = 1 }: Props) {
+  const { isWindowDragging, isWindowResizing, isActiveWindow } =
+    useWindowAction();
   const isDisabled = isWindowDragging || isWindowResizing;
 
   return (
-    <iframe
-      src={url}
-      className={`w-full h-full bg-widget-80 ${
-        isDisabled ? "pointer-events-none select-none touch-none" : ""
-      }`}
-      aria-disabled={isDisabled}
-    />
+    <div className="w-full h-full bg-widget-80 relative">
+      <iframe
+        src={url}
+        className={`w-full h-full bg-none bg-transparent relative ${
+          isDisabled ? "pointer-events-none select-none touch-none" : ""
+        } ${className}`}
+        aria-disabled={isDisabled}
+        style={{ zoom }}
+      />
+      <div
+        className={`absolute w-full h-full left-0 top-0 ${
+          isActiveWindow
+            ? "hidden pointer-events-none select-none touch-none invisible"
+            : ""
+        }`}
+      />
+    </div>
   );
 }
