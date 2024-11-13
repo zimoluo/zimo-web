e56a116e16b58af83fa91f73f5838d706777e238
zimoluo <zimo_luo@outlook.com>
2024-11-13 17:06:12 -0500

    changed to use backdrop gradient for favicon

--- components/theme/config/birthday19.ts
+++ components/theme/config/birthday19.ts
@@ -95,16 +95,282 @@ const birthday19Config: ThemeDataConfig = {
   },
   siteThemeColor: "#16304d",
   favicon: {
-    mode: "separate",
+    mode: "backdrop",
     outline: "#d0d9e6",
-    gradient: [
+    backdropGradient: [
       {
+        type: "radial-gradient",
+        sizeX: 42.68,
+        sizeY: 42.72,
+        posX: 20.1,
+        posY: 120.2,
         stops: [
-          { offset: 0, color: "#143d62" },
-          { offset: 0.34, color: "#173251" },
-          { offset: 0.76, color: "#054757" },
-          { offset: 1, color: "#083d4b" },
+          {
+            at: 0,
+            opacity: 1,
+            color: [117, 21, 85],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [91, 19, 20],
+          },
         ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 35.16,
+        sizeY: 35.21,
+        posX: 93.3,
+        posY: -17.4,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [30, 119, 137],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 80],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 50.54,
+        sizeY: 50.54,
+        posX: 6.0,
+        posY: 31.7,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [129, 43, 19],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [91, 34, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 36.08,
+        sizeY: 36.08,
+        posX: 92.4,
+        posY: 29.4,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [169, 112, 32],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [91, 67, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 25.39,
+        sizeY: 25.44,
+        posX: 117.8,
+        posY: 97.8,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [30, 86, 137],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 80],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 35.99,
+        sizeY: 36.08,
+        posX: 47.6,
+        posY: 116.0,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [146, 68, 26],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [91, 67, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 31.1,
+        sizeY: 31.05,
+        posX: 118.1,
+        posY: 80.0,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [129, 84, 19],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [74, 91, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 36.38,
+        sizeY: 36.43,
+        posX: 62.4,
+        posY: 47.0,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [14, 98, 64],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 63],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 42.14,
+        sizeY: 42.14,
+        posX: 40.0,
+        posY: -12.7,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [18, 88, 39],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 63],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 28.61,
+        sizeY: 28.61,
+        posX: 84.0,
+        posY: 120.3,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [21, 117, 79],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [46, 91, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 28.66,
+        sizeY: 28.66,
+        posX: 6.1,
+        posY: 111.1,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [21, 117, 79],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [46, 91, 19],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 34.72,
+        sizeY: 34.72,
+        posX: 3.4,
+        posY: 79.3,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [117, 21, 49],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [91, 19, 90],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 28.61,
+        sizeY: 28.61,
+        posX: 18.7,
+        posY: 82.3,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [30, 86, 137],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 80],
+          },
+        ],
+      },
+      {
+        type: "radial-gradient",
+        sizeX: 25.54,
+        sizeY: 25.39,
+        posX: 109.4,
+        posY: 26.3,
+        stops: [
+          {
+            at: 0,
+            opacity: 1,
+            color: [30, 86, 137],
+          },
+          {
+            at: 100,
+            opacity: 0,
+            color: [19, 91, 80],
+          },
+        ],
+      },
+      {
+        type: "linear-gradient",
+        stops: [
+          { color: [21, 53, 89], opacity: 1, at: 20 },
+          { color: [22, 48, 77], opacity: 1, at: 80 },
+        ],
+        angle: 45,
       },
     ],
   },
