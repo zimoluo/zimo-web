b9fa99472104fac3da600390171cc1be3b4198c7
zimoluo <zimo_luo@outlook.com>
2024-11-21 17:24:53 -0500

    add fireworks cuz why not

--- components/mainPage/backgroundAnimations/birthday19/Birthday19AnimatedBackground.tsx
+++ components/mainPage/backgroundAnimations/birthday19/Birthday19AnimatedBackground.tsx
@@ -5,12 +5,16 @@ import nineteenSource from "@/public/theme/animated-background/birthday19/ninete
 import colorsSource from "@/public/theme/animated-background/birthday19/colors.svg";
 import birthday19Style from "./birthday19.module.css";
 import { useSettings } from "@/components/contexts/SettingsContext";
+import CelebrationAnimatedBackground from "../celebration/CelebrationAnimatedBackground";
 
 export default function Birthday19AnimatedBackground() {
   const { settings } = useSettings();
 
   return (
     <>
+      {settings.backgroundRichness === "rich" && (
+        <CelebrationAnimatedBackground />
+      )}
       <div className="fixed -z-20 inset-0 flex items-center justify-center w-screen h-screen pointer-events-none select-none touch-none">
         <Image
           src={colorsSource}
