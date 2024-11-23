8eb6dc2f47a482b2e96dea61b18b4ab1ec31bdff
zimoluo <zimo_luo@outlook.com>
2024-11-22 21:55:44 -0500

    i dont wanna see you i dont wanna miss you again

--- app/christmas-tree/ChristmasTreeSelectButton.tsx
+++ app/christmas-tree/ChristmasTreeSelectButton.tsx
@@ -45,7 +45,7 @@ export default function ChristmasTreeSelectButton({ sprite }: Props) {
     >
       <Image
         src={`https://zimo-web-bucket.s3.us-east-2.amazonaws.com/special/christmas/public/sprites/${sprite}.svg`}
-        className="w-full h-full object-contain aspect-square transition-transform duration-300 group-hover:scale-110 ease-in-out drop-shadow-md"
+        className="w-full h-full object-contain aspect-square transition-transform duration-300 group-hover:scale-110 ease-in-out drop-shadow-md select-none"
         alt={`Pick this sprite: ${sprite}`}
         width={100}
         height={100}
