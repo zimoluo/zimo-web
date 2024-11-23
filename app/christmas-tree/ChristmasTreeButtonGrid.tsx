383d325ce4acecf8b0128446f4d8293f329d0615
zimoluo <zimo_luo@outlook.com>
2024-11-22 21:17:25 -0500

    add a scroll button

--- app/christmas-tree/ChristmasTreeButtonGrid.tsx
+++ app/christmas-tree/ChristmasTreeButtonGrid.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import UpDownSwitchIcon from "@/components/assets/entries/UpDownSwitchIcon";
 import ChristmasTreeSelectButton from "./ChristmasTreeSelectButton";
 
 const availableSprites = [
@@ -29,10 +32,18 @@ const availableSprites = [
 
 export default function ChristmasTreeButtonGrid() {
   return (
-    <section className="flex gap-4 md:gap-6 md:flex-col overflow-auto px-4 py-4 shrink-0">
-      {availableSprites.map((sprite, index) => (
-        <ChristmasTreeSelectButton key={index} sprite={sprite} />
-      ))}
-    </section>
+    <div className="relative md:h-full">
+      <section className="flex gap-4 md:gap-6 md:flex-col overflow-auto px-4 py-4 shrink-0 relative h-full">
+        {availableSprites.map((sprite, index) => (
+          <ChristmasTreeSelectButton key={index} sprite={sprite} />
+        ))}
+      </section>
+      <button className="md:hidden absolute left-4 w-9 px-2 h-12 top-1/2 -translate-y-1/2 bg-pastel bg-opacity-50 backdrop-blur-lg shadow-md rounded-xl">
+        <UpDownSwitchIcon className="-rotate-90 w-full h-auto aspect-square" />
+      </button>
+      <button className="md:hidden absolute right-4 w-9 px-2 h-12 top-1/2 -translate-y-1/2 bg-pastel bg-opacity-50 backdrop-blur-lg shadow-md rounded-xl">
+        <UpDownSwitchIcon className="rotate-90 w-full h-auto aspect-square" />
+      </button>
+    </div>
   );
 }
