b853fd80b2b53227b018bbea1e40fad115ce59de
zimoluo <zimo_luo@outlook.com>
2024-11-24 14:54:17 -0500

    soft is now gone

--- components/mainPage/menu/settings/SettingsSlider.tsx
+++ components/mainPage/menu/settings/SettingsSlider.tsx
@@ -132,7 +132,9 @@ export default function SettingsSlider({
         handleSetPosition(positionPercent);
       }}
     >
-      <div className="absolute w-9/10 h-2 border-soft bg-pastel border-opacity-80 border-0.8 bg-opacity-30 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+      <div
+        className={`absolute w-9/10 h-2 ${sliderStyle.borderColor} bg-pastel border-0.8 bg-opacity-30 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2`}
+      >
         {values.map((value, key) => {
           const positionPercent = (100 / (values.length - 1)) * key;
           return (
@@ -170,7 +172,9 @@ export default function SettingsSlider({
           style={{ left: `${sliderPos}%` }}
         >
           <div
-            className={`border-soft border-0.6 shadow-md w-2.5 h-6 rounded-full transition-all ease-in-out -translate-x-1 -translate-y-2.25 touch-none ${
+            className={`${
+              sliderStyle.borderColor
+            } border-0.6 shadow-md w-2.5 h-6 rounded-full transition-all ease-in-out -translate-x-1 -translate-y-2.25 touch-none ${
               isDragging
                 ? "cursor-grabbing scale-150 bg-highlight"
                 : `cursor-grab scale-135 ${sliderStyle.sliderBg}`
