492f7663843e236a896452f79dc262ac939a8b67
zimoluo <zimo_luo@outlook.com>
2025-03-01 16:03:17 -0500

    add react-scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,6 +109,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      </head>
       <body
         className={`${mainFont.variable} ${monoFont.variable} ${tabularFont.variable} ${serifFont.variable} ${fancyFont.variable} font-main`}
       >
