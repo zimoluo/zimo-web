3b46a57a9805844ed0e5e23dca9a97645c2134f6
zimoluo <zimo_luo@outlook.com>
2025-03-02 15:31:53 -0500

    updated verdant

--- components/theme/config/verdant.ts
+++ components/theme/config/verdant.ts
@@ -1,52 +1,42 @@
 const verdantConfig: ThemeDataConfig = {
   palette: {
-    primary: [82, 116, 12],
-    saturated: [121, 166, 29],
-    pastel: [222, 255, 102],
-    light: [243, 255, 197],
+    primary: [36, 69, 78],
+    saturated: [66, 128, 135],
+    pastel: [200, 224, 218],
+    light: [240, 255, 244],
     page: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
-          {
-            color: [142, 223, 34],
-            opacity: 1,
-            at: 0,
-          },
-          {
-            color: [142, 223, 34],
-            opacity: 1,
-            at: 100,
-          },
+          { color: [153, 193, 194], opacity: 1, at: 0 },
+          { color: [153, 193, 194], opacity: 1, at: 100 },
         ],
+        angle: 90,
       },
     ],
     widget: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
           {
-            color: [232, 255, 164],
-            opacity: 1.0,
+            color: [226, 238, 236],
+            opacity: 1,
             isWidgetOpacity: true,
             at: 0,
           },
           {
-            color: [232, 255, 164],
-            opacity: 1.0,
+            color: [226, 238, 236],
+            opacity: 1,
             isWidgetOpacity: true,
             at: 100,
           },
         ],
+        angle: 30,
       },
     ],
   },
-  siteThemeColor: "#8edf22",
-  favicon: {
-    mode: "backdrop",
-  },
+  siteThemeColor: "#99c1c2",
+  favicon: { mode: "backdrop" },
   animatedBackgroundKey: "verdant",
 };
 
