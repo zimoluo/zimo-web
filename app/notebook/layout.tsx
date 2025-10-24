45b23c1053d258ef4df44a6e0b6390d0157ec842
zimoluo <zimo_luo@outlook.com>
2025-10-23 20:45:19 -0400

    readability

--- app/notebook/layout.tsx
+++ app/notebook/layout.tsx
@@ -18,7 +18,7 @@ export default function NotebookLayout({ children }: Props) {
   return (
     <div className="w-screen h-screen min-h-[25rem] flex justify-center">
       <div
-        className={`${notebookPageStyle.container} mt-14 md:my-18 shadow-xl md:rounded-[2rem] backdrop-blur-[6px] overflow-hidden`}
+        className={`${notebookPageStyle.container} mt-14 md:my-18 shadow-xl md:rounded-[2rem] backdrop-blur-[24px] overflow-hidden`}
       >
         <WindowNotebook />
       </div>
