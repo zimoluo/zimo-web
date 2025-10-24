a6b4378a65a26603c25ff304f2b9feb8e9b99300
zimoluo <zimo_luo@outlook.com>
2025-10-24 02:43:18 -0400

    fixes

--- components/mainPage/menu/MenuEntriesNavigation.tsx
+++ components/mainPage/menu/MenuEntriesNavigation.tsx
@@ -25,14 +25,20 @@ export default function MenuEntriesNavigation({
       }`}
       className="h-12 rounded-full bg-[color-mix(rgb(var(--color-pastel)_/_0.5)_0.45,rgb(var(--color-light)_/_0.5))] p-2.5 shadow-sm border-reflect-pastel"
     >
-      <div className="group cursor-pointer flex items-center gap-3 relative">
+      <div className="group cursor-pointer flex items-center justify-start gap-3 relative">
         <MenuNavigationEntryGlow item={item} />
         <NavigationIcon
           className={`relative h-7 w-auto aspect-square transition-transform duration-300 group-hover:scale-110 ${
             item === "home" ? "visible" : ""
           }`}
         />
-        <div className="absolute top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 w-full text-center">
+        <div
+          className={`${
+            item === "home"
+              ? "absolute top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 w-full"
+              : "relative"
+          } md:absolute md:top-1/2 md:-translate-x-1/2 md:left-1/2 md:-translate-y-1/2 md:w-full text-center`}
+        >
           <MenuNavigationEntryText item={item} />
         </div>
       </div>
