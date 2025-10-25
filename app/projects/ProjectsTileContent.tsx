e586bacb5084b3024708f0e865133618360a12c9
zimoluo <zimo_luo@outlook.com>
2025-10-25 17:26:06 -0400

    tweak project tile

--- app/projects/ProjectsTileContent.tsx
+++ app/projects/ProjectsTileContent.tsx
@@ -21,7 +21,7 @@ export default function ProjectsTileContent({
         className="h-12 md:h-20 w-auto transition-transform ease-in duration-200 group-hover:scale-110 drop-shadow-lg"
         alt={title}
       />
-      <p className="absolute top-26 md:top-40 left-1/2 -translate-x-1/2 w-44 text-center font-bold text-saturated transition-all text-sm md:text-base ease-in duration-200 opacity-60 md:opacity-0 md:group-hover:opacity-60 md:group-hover:top-38">
+      <p className="absolute top-26 md:top-40 left-1/2 -translate-x-1/2 w-44 text-center font-bold text-saturated transition-all text-[0.83rem] leading-5 md:text-base ease-in duration-200 opacity-60 md:opacity-0 md:group-hover:opacity-60 md:group-hover:top-38">
         {title}
       </p>
     </>
