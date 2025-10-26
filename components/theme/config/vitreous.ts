dbe59d9af996758468b64d961c0626aa6acba937
zimoluo <zimo_luo@outlook.com>
2025-10-26 13:57:26 -0400

    vitreous theme rework

--- components/theme/config/vitreous.ts
+++ components/theme/config/vitreous.ts
@@ -1,139 +1,91 @@
 const vitreousConfig: ThemeDataConfig = {
   palette: {
     primary: [243, 242, 255],
-    saturated: [184, 198, 255],
-    pastel: [95, 89, 255],
-    light: [85, 66, 255],
+    saturated: [175, 180, 255],
+    pastel: [118, 116, 240],
+    light: [102, 65, 235],
     page: [
       {
         type: "repeating-linear-gradient",
-        angle: 315,
         stops: [
-          {
-            color: [0, 255, 255],
-            opacity: 0.1804,
-            at: 92,
-          },
-          {
-            color: [7, 58, 255],
-            opacity: 0.0,
-            at: 100,
-          },
+          { color: [0, 255, 255], opacity: 0.1804, at: 92 },
+          { color: [7, 58, 255], opacity: 0, at: 100 },
         ],
+        angle: 315,
       },
       {
         type: "repeating-radial-gradient",
-        sizeX: 75,
-        sizeY: 75,
+        stops: [
+          { color: [0, 255, 255], opacity: 0.0706, at: 30 },
+          { color: [7, 58, 255], opacity: 0.0784, at: 39 },
+        ],
         posX: 238,
         posY: 218,
-        stops: [
-          {
-            color: [0, 255, 255],
-            opacity: 0.0706,
-            at: 30,
-          },
-          {
-            color: [7, 58, 255],
-            opacity: 0.0784,
-            at: 39,
-          },
-        ],
+        sizeX: 75,
+        sizeY: 75,
       },
       {
         type: "radial-gradient",
-        sizeX: 99,
-        sizeY: 99,
+        stops: [
+          { color: [0, 201, 255], opacity: 1, at: 0 },
+          { color: [7, 58, 255], opacity: 0, at: 100 },
+        ],
         posX: 109,
         posY: 2,
-        stops: [
-          {
-            color: [0, 201, 255],
-            opacity: 1.0,
-            at: 0,
-          },
-          {
-            color: [7, 58, 255],
-            opacity: 0.0,
-            at: 100,
-          },
-        ],
-      },
-      {
-        type: "radial-gradient",
         sizeX: 99,
         sizeY: 99,
-        posX: 21,
-        posY: 78,
-        stops: [
-          {
-            color: [123, 0, 255],
-            opacity: 1.0,
-            at: 0,
-          },
-          {
-            color: [7, 58, 255],
-            opacity: 0.0,
-            at: 100,
-          },
-        ],
       },
       {
         type: "radial-gradient",
-        sizeX: 160,
-        sizeY: 154,
+        stops: [
+          { color: [123, 0, 255], opacity: 1, at: 0 },
+          { color: [7, 58, 255], opacity: 0, at: 100 },
+        ],
+        posX: 21,
+        posY: 78,
+        sizeX: 99,
+        sizeY: 99,
+      },
+      {
+        type: "radial-gradient",
+        stops: [
+          { color: [79, 53, 230], opacity: 1, at: 0 },
+          { color: [38, 96, 255], opacity: 1, at: 100 },
+        ],
         posX: 0,
         posY: 100,
-        stops: [
-          {
-            color: [79, 53, 230],
-            opacity: 1,
-            at: 0,
-          },
-          {
-            color: [38, 96, 255],
-            opacity: 1,
-            at: 100,
-          },
-        ],
+        sizeX: 160,
+        sizeY: 154,
       },
     ],
     pageMinimal: [
       {
         type: "linear-gradient",
-        angle: 45,
         stops: [
-          {
-            color: [79, 53, 230],
-            opacity: 1,
-            at: 15,
-          },
-          {
-            color: [38, 96, 255],
-            opacity: 1,
-            at: 85,
-          },
+          { color: [79, 53, 230], opacity: 1, at: 15 },
+          { color: [38, 96, 255], opacity: 1, at: 85 },
         ],
+        angle: 45,
       },
     ],
     widget: [
       {
         type: "linear-gradient",
-        angle: 90,
         stops: [
           {
-            color: [56, 109, 242],
-            opacity: 1.0,
+            color: [117, 64, 255],
+            opacity: 0.89,
             isWidgetOpacity: true,
-            at: 15,
+            at: 12.5,
           },
           {
-            color: [59, 104, 255],
-            opacity: 1.0,
+            color: [59, 83, 255],
+            opacity: 0.97,
             isWidgetOpacity: true,
-            at: 85,
+            at: 100,
           },
         ],
+        angle: 0,
       },
     ],
   },
@@ -143,14 +95,8 @@ const vitreousConfig: ThemeDataConfig = {
     gradient: [
       {
         stops: [
-          {
-            color: "#883dff",
-            offset: 0,
-          },
-          {
-            color: "#3068ff",
-            offset: 1,
-          },
+          { color: "#883dff", offset: 0 },
+          { color: "#3068ff", offset: 1 },
         ],
       },
     ],
