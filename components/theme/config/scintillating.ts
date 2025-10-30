8fbe990b90f9aeedae2d785016863bce6a62f8f6
zimoluo <zimo_luo@outlook.com>
2025-10-30 17:09:56 -0400

    new scintillating

--- components/theme/config/scintillating.ts
+++ components/theme/config/scintillating.ts
@@ -1,23 +1,23 @@
 const scintillatingConfig: ThemeDataConfig = {
   palette: {
-    primary: [255, 245, 237],
-    saturated: [255, 212, 138],
-    pastel: [255, 181, 52],
-    light: [241, 126, 7],
+    primary: [120, 0, 50],
+    saturated: [194, 0, 94],
+    pastel: [255, 152, 198],
+    light: [255, 217, 228],
     page: [
       {
         type: "repeating-linear-gradient",
         stops: [
-          { color: [214, 173, 25], opacity: 0.18, at: 92 },
-          { color: [224, 110, 16], opacity: 0, at: 100 },
+          { color: [255, 143, 184], opacity: 0.18, at: 92 },
+          { color: [255, 131, 157], opacity: 0, at: 100 },
         ],
         angle: 315,
       },
       {
         type: "repeating-radial-gradient",
         stops: [
-          { color: [255, 218, 83], opacity: 0.0706, at: 30 },
-          { color: [255, 119, 7], opacity: 0.0784, at: 39 },
+          { color: [255, 186, 221], opacity: 0.0706, at: 30 },
+          { color: [255, 56, 96], opacity: 0.0784, at: 39 },
         ],
         posX: 238,
         posY: 218,
@@ -27,8 +27,8 @@ const scintillatingConfig: ThemeDataConfig = {
       {
         type: "radial-gradient",
         stops: [
-          { color: [231, 176, 21], opacity: 1, at: 0 },
-          { color: [255, 138, 8], opacity: 0, at: 100 },
+          { color: [255, 212, 234], opacity: 1, at: 0 },
+          { color: [255, 140, 186], opacity: 0, at: 100 },
         ],
         posX: 109,
         posY: 2,
@@ -38,8 +38,8 @@ const scintillatingConfig: ThemeDataConfig = {
       {
         type: "radial-gradient",
         stops: [
-          { color: [203, 79, 21], opacity: 1, at: 0 },
-          { color: [255, 83, 8], opacity: 0, at: 100 },
+          { color: [255, 212, 221], opacity: 1, at: 0 },
+          { color: [255, 222, 239], opacity: 0, at: 100 },
         ],
         posX: 21,
         posY: 78,
@@ -49,8 +49,8 @@ const scintillatingConfig: ThemeDataConfig = {
       {
         type: "radial-gradient",
         stops: [
-          { color: [211, 101, 42], opacity: 1, at: 0 },
-          { color: [239, 157, 69], opacity: 1, at: 100 },
+          { color: [255, 212, 227], opacity: 1, at: 0 },
+          { color: [255, 222, 229], opacity: 1, at: 100 },
         ],
         posX: 0,
         posY: 100,
@@ -62,8 +62,8 @@ const scintillatingConfig: ThemeDataConfig = {
       {
         type: "linear-gradient",
         stops: [
-          { color: [211, 101, 42], opacity: 1, at: 15 },
-          { color: [239, 157, 69], opacity: 1, at: 85 },
+          { color: [255, 201, 228], opacity: 1, at: 15 },
+          { color: [255, 206, 224], opacity: 1, at: 85 },
         ],
         angle: 45,
       },
@@ -72,22 +72,28 @@ const scintillatingConfig: ThemeDataConfig = {
       {
         type: "linear-gradient",
         stops: [
-          { color: [241, 143, 14], opacity: 1, isWidgetOpacity: true, at: 15 },
-          { color: [231, 152, 49], opacity: 1, isWidgetOpacity: true, at: 85 },
+          { color: [255, 199, 212], opacity: 1, isWidgetOpacity: true, at: 0 },
+          {
+            color: [255, 218, 236],
+            opacity: 1,
+            isWidgetOpacity: true,
+            at: 100,
+          },
         ],
-        angle: 90,
+        angle: 0,
       },
     ],
   },
-  siteThemeColor: "#ff8f17",
+  siteThemeColor: "#ff92be",
   favicon: {
     mode: "separate",
     gradient: [
       {
         stops: [
-          { color: "#e14b0b", offset: 0 },
-          { color: "#ffc325", offset: 1 },
+          { offset: 0, color: "#ffd1e9" },
+          { offset: 0.8, color: "#ffa6c5" },
         ],
+        angle: 90,
       },
     ],
   },
