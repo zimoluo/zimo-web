1905d6d3f116e4cf592d786218b5776522e4ff57
zimoluo <zimo_luo@outlook.com>
2025-11-06 20:16:36 -0500

    things that were forgotten

--- components/widgets/ReadingLayout.tsx
+++ components/widgets/ReadingLayout.tsx
@@ -9,7 +9,7 @@ export default function ReadingLayout({ children }: Props) {
   return (
     <div className="flex justify-center items-center">
       <article
-        className={`relative mt-[68px] sm:mt-[72px] mb-4 md:mb-20 md:mx-8 px-6 bg-widget-90 ${readingLayoutStyle.sizing} md:px-14 pb-12 pt-16 rounded-b-[2rem] md:rounded-[2rem] md:shadow-xl`}
+        className={`relative mt-[68px] sm:mt-[72px] mb-4 md:mb-20 md:mx-8 px-6 bg-widget-90 ${readingLayoutStyle.sizing} md:px-14 pb-12 pt-16 rounded-b-[2rem] md:rounded-[2rem] md:shadow-xl outline outline-1 outline-highlight-light/15`}
       >
         {children}
       </article>
