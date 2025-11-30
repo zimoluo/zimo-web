40b00b70bdef381897ca3280d6769af66907fa03
zimoluo <zimo_luo@outlook.com>
2025-11-30 13:57:51 -0500

    add the round

--- app/notebook/layout.tsx
+++ app/notebook/layout.tsx
@@ -18,7 +18,7 @@ export default function NotebookLayout({ children }: Props) {
   return (
     <div className="w-screen h-screen min-h-[25rem] flex justify-center">
       <div
-        className={`${notebookPageStyle.container} mt-[68px] sm:mt-[72px] shadow-xl md:rounded-[2rem] backdrop-blur-reading overflow-hidden md:outline md:outline-1 md:outline-highlight-light/15`}
+        className={`${notebookPageStyle.container} mt-[68px] sm:mt-[72px] shadow-xl rounded-b-[2rem] md:rounded-[2rem] backdrop-blur-reading overflow-hidden md:outline md:outline-1 md:outline-highlight-light/15`}
       >
         <WindowNotebook />
       </div>
